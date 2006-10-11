61b6044d89f9b66279a53b16621146892fb642f4
Sam Lindley <Sam.Lindley@ed.ac.uk>
2006-10-11 18:44:40 +0000

    create branch for the charlist fix

--- json.ml
+++ json.ml
@@ -2,7 +2,7 @@ let jsonize_primitive : Result.primitive_value -> string = function
   | `Bool value -> string_of_bool value
   | `Int value -> Num.string_of_num value
   | `Float value -> string_of_float value
-  | `Char c -> "'"^ Char.escaped c ^"'"
+  | `Char c -> "\"" ^ String.escaped(Char.escaped c) ^"\"" (* FIXME: what does [escaped] escape? *)
   | `XML _
   | `Table _
   | `Database _ as p -> prerr_endline ("Can't yet jsonize " ^ Result.string_of_primitive p); ""
@@ -14,8 +14,10 @@ let rec jsonize_result : Result.result -> string = function
   | `List ((`XML _)::_)
   | `Function _ as r -> prerr_endline ("Can't yet jsonize " ^ Result.string_of_result r); ""
   | #Result.primitive_value as p -> jsonize_primitive p
-  | `Record fields -> "{" ^ String.concat ", " (List.map (fun (k, v) -> "\"" ^ k ^ "\" : " ^ jsonize_result v) fields) ^ "}"
+  | `Record fields -> "{" ^ String.concat ", " (List.map (fun (kj, v) -> "\"" ^ kj ^ "\" : " ^ jsonize_result v) fields) ^ "}"
   | `List [] -> "[]"
-  | `List ((`Char _)::_) as c  -> "\"" ^ Result.escape (Result.charlist_as_string c) ^ "\""
-  | `List (elems) -> "[" ^ String.concat ", " (List.map jsonize_result elems) ^ "]"
+(*   | `List ((`Char _)::_) as c  -> "\"" ^ Result.escape (Result.charlist_as_string c) ^ "\"" *)
+  | `List (elems) ->
+(*      Debug.debug ("jsonizing list: " ^ Result.Show_result.show (`List (elems)));*)
+      "[" ^ String.concat ", " (List.map jsonize_result elems) ^ "]"
 
