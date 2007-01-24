6ff3b93e143d1a15384e5fd4516fcdce048a6bc9
Sam Lindley <Sam.Lindley@ed.ac.uk>
2007-01-24 15:05:41 +0000

    fix reference to Types.primitive

--- sugartypes.mli
+++ sugartypes.mli
@@ -35,7 +35,7 @@ type datatype =
   | TableType of row
   | ListType of datatype
   | TypeApplication of (string * datatype list)
-  | PrimitiveType of Types.primitive
+  | PrimitiveType of Type_basis.primitive
   | DBType
 and row = (string * [`Present of datatype | `Absent]) list * string option
 
