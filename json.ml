20e67f7ddfd7429ba5d0efee3f04b49b5aae0991
Sam Lindley <Sam.Lindley@ed.ac.uk>
2007-03-01 13:46:19 +0000

    Implement jsonisation of XML values.
    We still need to implement the corresponding parser for reading in
    jsonised values at the server. But I don't yet need that functionality
    for the application I'm currently working on :-)

--- json.ml
+++ json.ml
@@ -13,19 +13,36 @@ let json_of_db (db, params) =
     "{ _db : { driver : '" ^ driver ^ "', name : '" ^ name ^ "', args : '" ^ args ^ "'} }"
 
 
+let jscharlist_of_string s =
+  "["^String.concat ", " (List.map (fun c -> "\"" ^ Char.escaped c ^ "\"") (Utility.StringUtils.explode s))^"]"
+
+let rec json_of_xmlitem = function
+  | Result.Text s ->
+      "[\"TEXT\", \"" ^ String.escaped (s) ^ "\"]"
+  | Result.Node (tag, xml) ->
+      let attrs, body =
+        List.fold_right (fun xmlitem (attrs, body) ->
+                           match xmlitem with
+                             | Result.Attr (label, value) ->
+                                 ("\"" ^label ^ "\" : " ^ jscharlist_of_string(value)) :: attrs, body
+                             | _ -> attrs, (json_of_xmlitem xmlitem) :: body) xml ([], [])
+      in
+        "[\"ELEMENT\", \"" ^ tag ^ "\", {" ^ String.concat ", " attrs
+        ^"}, [" ^ String.concat ", " body ^ "]]"
+  | Result.Attr _ -> assert false
+
 let jsonize_primitive : Result.primitive_value -> string = function
   | `Bool value -> string_of_bool value
   | `Int value -> Num.string_of_num value
   | `Float value -> string_of_float value
   | `Char c -> "\"" ^ String.escaped (Char.escaped c) ^"\"" (* FIXME: what does [escaped] escape? *)
-  | `XML _
   | `Table _ as p -> prerr_endline ("Can't yet jsonize " ^ Result.string_of_primitive p); ""
   | `Database db -> json_of_db db
+  | `XML xmlitem -> json_of_xmlitem xmlitem
 
 let rec jsonize_result : Result.result -> string = function
   | `PFunction _
   | `Continuation _
-  | `List ((`XML _)::_)
   | `Function _ as r -> prerr_endline ("Can't yet jsonize " ^ Result.string_of_result r); ""
   | #Result.primitive_value as p -> jsonize_primitive p
   | `Variant (label, value) -> Printf.sprintf "{\"_label\" : \"%s\",\"_value\":%s}" label (jsonize_result value)
