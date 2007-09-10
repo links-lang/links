9b86daabcd0a5a607e3c7eee2fcb62325fbe6e83
Sam Lindley <Sam.Lindley@ed.ac.uk>
2007-09-10 13:33:41 +0000

    Fix bug with nullary for comprehensions - the unit of cartesian product is 1 not 0.

--- sugar.ml
+++ sugar.ml
@@ -1310,7 +1310,14 @@ module Desugarer =
                              | `Foreign _ -> assert false (* TODO *)) : binding -> _) es in
                  polylets es (desugar exp)
 
-           | `Iteration ([], _, _, _) ->  Nil pos
+           | `Iteration ([], body, filter, None) ->
+               let body =
+                 match filter with
+                   | None -> body
+                   | Some condition ->
+                       `Conditional (condition, body, (`ListLit [], pos')), pos'
+               in
+                 desugar body
            | `Iteration (generators, body, filter, None) ->
                let body =
                  match filter with
