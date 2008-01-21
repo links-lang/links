links-lang/links

commit 4b56b3520cb6cf80a9e6c6524c2cfc5ae8237f4d

Rationalised date and time stuff.
We now have four functions:

  clientTime : () -> Int
  serverTime : () -> Int

  intToDate : (Int) -> (year:Int, month:Int, day:Int, hours:Int,
minutes:Int, seconds:Int)
  dateToInt : ((year:Int, month:Int, day:Int, hours:Int, minutes:Int,
seconds:Int)) -> Int

The first two functions return the number of seconds since 1970 from
the client and server respectively.

The other functions convert back and forth between the number of
seconds since 1970 and a record representation. The record
representation is based on that used by JavaScript.

The ranges of fields are as follows

  year (0-)
  month (0-11)
  day (1-31)
  hours (0-23)
  minutes (0-59)
  seconds (0-59)

The representation differs slightly from that of UNIX in that UNIX
years start from 1900 (i.e. in UNIX 1900 is represented in 0 whereas
in JavaScript and now Links 1900 is represented as 1900).



git-svn-id: http://groups.inf.ed.ac.uk/links/svn/trunk@1437 f10b8b52-1f0f-0410-b9dc-dace7d27bc78

lib/js/jslib.js
@@ -1361,8 +1361,31 @@ function _activateStyleElement() {
   _cssText = null;
 }
 
-function _time () { return (new Date()).getTime(); } 
-var getCurrentTime = LINKS.kify(_time);
+// time in seconds since the beginning of 1970
+function _clientTime () { return (new Date()).getTime(); } 
+var clientTime = LINKS.kify(_clientTime);
+
+function _dateToLinksDate(d) {
+  return {year:d.getFullYear(), month:d.getMonth(), day:d.getDate(),
+        hours:d.getHours(), minutes:d.getMinutes(), seconds:d.getSeconds()};
+}
+
+function _linksDateToDate(d) {
+  return new Date(d.year, d.month, d.day, d.hours, d.minutes, d.seconds);
+}
+
+// convert seconds since beginning of 1970 to a date
+function _intToDate(t) {
+  return _dateToLinksDate(new Date(t * 1000));
+}
+var intToDate = Links.kify(_intToDate);
+
+// convert a date to seconds since beginning of 1970
+function _dateToInt(date) {
+  return Math.floor(_linksDateToDate(date).getTime() / 1000);
+}
+var dateToInt = Links.kify(_dateToInt);
+
 
 var _pageTimer;
 

examples/citations.links
@@ -217,7 +217,7 @@ fun distinct(xs) {
               # because JavaScript is so slow that sending the
               # message back and forth has the desired effect
               # but this is what the code should look like
-              var u = getCurrentTime();        
+              var u = serverTime();        
               if(u-t < w) { sleep(w-(u-t)) } else {()};
 
               handler!GetCurrentState;
@@ -236,7 +236,7 @@ fun distinct(xs) {
               error("Didn't ask for a suggestion!");
           }
         }
-        var t = getCurrentTime();
+        var t = serverTime();
         suggester("", "", t)
       }
     }

examples/extras.js
@@ -17,7 +17,7 @@ function _mandelbrot(x, y) {
   }
   return 0;
 }
-var jsmandelbrot =LINKS.kify(_mandelbrot, 2);
+var jsmandelbrot =LINKS.kify(_mandelbrot);
 
 function _getCanvasById(id) {
   var node =_getNodeById(id);
@@ -29,9 +29,9 @@ function _canvasSetFillStyle(context, colour) {
 function _canvasFillRect(context, x, y, width, height) {
   context.fillRect(x, y, width, height);
 }
-var getCanvasById = LINKS.kify(_getCanvasById, 1);
-var canvasSetFillStyle = LINKS.kify(_canvasSetFillStyle, 2);
-var canvasFillRect = LINKS.kify(_canvasFillRect, 5);
+var getCanvasById = LINKS.kify(_getCanvasById);
+var canvasSetFillStyle = LINKS.kify(_canvasSetFillStyle);
+var canvasFillRect = LINKS.kify(_canvasFillRect);
 
 
 function _plot(context, x, y) {
@@ -67,7 +67,7 @@ function _fullyNativeMandelbrot() {
   var context = _getCanvasById(_stringToCharlist(id));
   context.fillStyle = "red";
   
-  var startTime = _getCurrentTime();
+  var startTime = _clientTime();
 
   for (y = -39; y <= 39; y++) {
     for (x = -39; x <= 39; x++) {
@@ -76,7 +76,7 @@ function _fullyNativeMandelbrot() {
     }
   }
 
-  var endTime = _getCurrentTime();
+  var endTime = _clientTime();
   var totalTime = (endTime-startTime);
 
   div.innerHTML = totalTime+"ms";
@@ -85,10 +85,4 @@ function _fullyNativeMandelbrot() {
   //domInsertBefore(enxml(string_of_int(totalTime)++"ms"), getNodeById(id));
   //debug("Time to draw "++id++": "++string_of_int(totalTime)++"ms") 
 }
-var fullyNativeMandelbrot = LINKS.kify(_fullyNativeMandelbrot, 0);
-
-
-function _getCurrentTime() {
-  return (new Date()).getTime();
-}
-var getCurrentTime = LINKS.kify(_getCurrentTime, 0);
+var fullyNativeMandelbrot = LINKS.kify(_fullyNativeMandelbrot);

examples/mandeltest.links
@@ -3,7 +3,6 @@
 typename Context = Int;
 
 alien javascript jsmandelbrot : (Float, Float) -> Int;
-alien javascript getCurrentTime : () -> Int;
 
 alien javascript getCanvasById : (String) -> Context;
 alien javascript canvasSetFillStyle : (Context, String) -> ();
@@ -163,7 +162,7 @@ fun drawings() {[("empty", nowt), ("line", line), ("block", block),
 fun dodrawing(drawingName, traversalName) client {
   var id = getfreshid("drawing");
   var c = canvas (80, 80, "red", id);
-  var startTime = getCurrentTime();
+  var startTime = clientTime();
 
 
   var drawing = lookupName(drawings(), drawingName);
@@ -172,7 +171,7 @@ fun dodrawing(drawingName, traversalName) client {
 
   traversal(drawing, c);
 
-  var endTime = getCurrentTime();
+  var endTime = clientTime();
   var totalTime = (endTime-startTime);
 
   insertBefore(stringToXml(intToString(totalTime)++"ms"), getNodeById(id));

examples/test-prims.links
@@ -246,9 +246,9 @@ ignore(getCookie("username"));
 
 sleep(1);
 
-# getCurrentTime   : () -> Int
+# serverTime   : () -> Int
 
-ignore(getCurrentTime());
+ignore(serverTime());
 
 # asList           : (TableHandle(c,b)) -> [c]
 # insertrows       : (TableHandle(c,b), [b]) -> ()

library.ml
@@ -771,20 +771,63 @@ let env : (string * (located_primitive * Types.datatype * pure)) list = [
      type system. *)
 
   "sleep",
-  (* FIXME: This isn't right : it freezes all threads *)
-  (p1 (fun duration -> Unix.sleep (int_of_num (unbox_int duration));
-         `Record []),
+  (p1 (fun duration ->
+         (* FIXME: This isn't right : it freezes all threads *)
+         (*Unix.sleep (int_of_num (unbox_int duration));
+         `Record []*)
+         failwith "The sleep function is not implemented on the server yet"
+      ),
    datatype "(Int) -> ()",
   IMPURE);
 
+  "clientTime",
+  (`Client,
+   datatype "() -> Int",
+   IMPURE);
+  
+  "serverTime",
+  (`Server
+     (`PFun (fun _ ->
+               box_int(num_of_int(int_of_float(Unix.time()))))),
+   datatype "() -> Int",
+   IMPURE);
 
-  (* [TODO]
-       Implement server version of the timer
-       (note: the OCaml unix time functionality is not portable
-       so we shouldn't use that)
-  *)
-  "getCurrentTime",
-  (`Client, datatype "() -> Int",
+  "dateToInt",
+  (p1 (fun r ->
+         match r with
+           | `Record r ->
+               let lookup s =
+                 int_of_num (unbox_int (List.assoc s r)) in
+               let tm = {
+                 Unix.tm_sec = lookup "seconds";
+   	         Unix.tm_min = lookup "minutes";
+   	         Unix.tm_hour = lookup "hours";
+   	         Unix.tm_mday = lookup "day";
+   	         Unix.tm_mon = lookup "month";
+   	         Unix.tm_year = (lookup "year" - 1900);
+   	         Unix.tm_wday = 0; (* ignored *)
+   	         Unix.tm_yday =  0; (* ignored *)
+   	         Unix.tm_isdst = false} in
+                 
+               let t, _ = Unix.mktime tm in
+                 box_int (num_of_int (int_of_float t))
+           | _ -> assert false),
+   datatype "((year:Int, month:Int, day:Int, hours:Int, minutes:Int, seconds:Int)) -> Int",
+   IMPURE);
+
+  "intToDate",
+  (p1 (fun t ->
+         let tm = Unix.localtime(float_of_int (int_of_num (unbox_int t))) in
+         let box_int = box_int -<- num_of_int in
+           `Record [
+             "year", box_int (tm.Unix.tm_year + 1900);
+             "month", box_int tm.Unix.tm_mon;
+             "day", box_int tm.Unix.tm_mday;
+             "hours", box_int tm.Unix.tm_hour;
+             "minutes", box_int tm.Unix.tm_min;
+             "seconds", box_int tm.Unix.tm_sec;
+           ]),
+  datatype "(Int) -> (year:Int, month:Int, day:Int, hours:Int, minutes:Int, seconds:Int)",
   IMPURE);
 
   (** Database functions **)

typeSugar.ml
@@ -1622,6 +1622,7 @@ and type_binding : context -> binding -> binding * Types.typing_environment =
                   let mt = Types.fresh_type_variable () in
                   let body = type_check {context with tenv = (Env.bind body_env (mailbox, mt), alias_env)} body in
                   let ft = make_ft pats (typ body) in
+                  (* WARNING: this looks surprising, but it is what we want *)
                   let ft' =
                     match Env.lookup body_env name with
                       | `ForAll (_, t) | t -> t in

commit d65715f57609f58cca60045deb37a35e7928e73a

Fix silly client bugs introduced in r1437.

lib/js/jslib.js
@@ -1378,22 +1378,22 @@ function _linksDateToDate(d) {
 function _intToDate(t) {
   return _dateToLinksDate(new Date(t * 1000));
 }
-var intToDate = Links.kify(_intToDate);
+var intToDate = LINKS.kify(_intToDate);
 
 // convert a date to seconds since beginning of 1970
 function _dateToInt(date) {
   return Math.floor(_linksDateToDate(date).getTime() / 1000);
 }
-var dateToInt = Links.kify(_dateToInt);
+var dateToInt = LINKS.kify(_dateToInt);
 
 
 var _pageTimer;
 
 function _startTimer() {
-  _pageTimer = _time();
+  _pageTimer = _clientTime();
 }
 function _stopTimer() {
-  _pageTimer = _time() - _pageTimer;
+  _pageTimer = _clientTime() - _pageTimer;
   _debug("Page drawn in " + _pageTimer + "ms");
 }
 