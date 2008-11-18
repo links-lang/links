links-lang/links

commit 4e852244c5ed0cad995b5609c5fa3256aff5ba8a

Added --precompile option to manually create a cache file. Added cache_directory setting to optionally choose a directory for   caches, with special naming behavior when this is on. Default   value "" turns it off, putting caches in the local directory.   (Note: presently caching only happens in web mode.) parse.ml now finds its own default preprocessor, rather than requiring   it to be passed in wherever parsing was called (all call sites used   the same default anyway). Factored the loading of the prelude in links.ml in anticipation of   further refactorings there at the top level.
git-svn-id: http://groups.inf.ed.ac.uk/links/svn/trunk@1554 f10b8b52-1f0f-0410-b9dc-dace7d27bc78

parse.ml
@@ -86,9 +86,8 @@ let program : (Sugartypes.binding list * Sugartypes.phrase option) grammar = Par
 let datatype : Sugartypes.datatype grammar = Parser.just_datatype
 
 let normalize_pp = function
-  | None
-  | Some "" -> None
-  | pp -> pp
+  | "" -> None
+  | pp -> Some pp
 
 (* We parse in a "context", which at the moment means the set of
    operator precedences, but more generally is an environment with
@@ -101,6 +100,8 @@ let normalize_context = function
   | None -> fresh_context ()
   | Some c -> c
 
+let default_preprocessor = (Settings.get_value Basicsettings.pp) 
+
 (** Public functions: parse some data source containing Links source
     code and return a list of ASTs. 
 
@@ -109,8 +110,8 @@ let normalize_context = function
     intercept and retain the code that has been read (in order to give
     better error messages).
 **)
-let parse_string ?pp ?in_context:context grammar string =
-  let pp = normalize_pp pp 
+let parse_string ?(pp=default_preprocessor) ?in_context:context grammar string =
+  let pp = normalize_pp pp
   and context = normalize_context context in 
     read ?nlhook:None ~parse:grammar ~infun:(reader_of_string ?pp string) ~name:"<string>" ~context
 
@@ -118,7 +119,7 @@ let parse_channel ?interactive ?in_context:context grammar (channel, name) =
   let context = normalize_context context in
     read ?nlhook:interactive ~parse:grammar ~infun:(reader_of_channel channel) ~name:name ~context
 
-let parse_file ?pp ?in_context:context grammar filename =
+let parse_file ?(pp=default_preprocessor) ?in_context:context grammar filename =
   match normalize_pp pp with
     | None -> parse_channel ?in_context:context grammar (open_in filename, filename)
     | Some pp ->

Makefile
@@ -111,7 +111,7 @@ SOURCES = $(OPC)                		\
           compilePatterns.ml                    \
           value.mli value.ml                    \
           compileir.ml                          \
-          loader.ml                             \
+          loader.mli loader.ml                  \
           irtojs.mli irtojs.ml                  \
           interpreter.mli interpreter.ml 	\
           evalir.ml                             \
@@ -161,7 +161,7 @@ quick-help:
 docs-clean:
 	cd doc && make clean
 
-prelude.links.cache: prelude.links
+prelude.links.cache: prelude.links links
 	@echo "Pre-compiling prelude..."
 	@./links -e 'print("Prelude compiled OK.")'
 

doc/quick-help.pod
@@ -1904,24 +1904,35 @@ renderMultipart : (Formlet (a), a -> Xml) -> Xml
 
 =head1 Running links
 
-Links can be run either as a CGI script through a web server such as Apache, or from the command line.
+Links can be run either as a CGI script through a web server such as
+Apache, or from the command line.
 
-When run as a CGI script Links must be passed a filename as an argument.
+When run as a CGI script Links must be passed a filename as an
+argument:
 
   links filename
 
-From the command line Links can be invoked in a number of different ways:
+The source code to evaluate can be specified in several ways:
 
-  links                load the interactive shell
-  links -e exp         evaluate exp
-  links filename       run filename
+  links                # run interactively
+  links -e exp         # evaluate exp, print its result
+  links filename       # run filename
 
-The following options are supported in all modes:
+Command line flags can be used to enable/disable some features:
 
-  -d                   debug mode
   -O                   enable optimisation
   -n                   don't print types
+  -d                   debug mode (for hacking on links itself)
+
+A configuration file can be chosen using this command-line option:
+
   --config=<filename>  set a configuration file
+
+These other options can be fun, too:
+
+  --pp=<command>          run all input through UNIX <command> before parsing
+  --precompile=<filename> precompile and store a cached version of <filename>
+  --dump=<filename>       find and print out the cached version of <filename>
 
 =head2 The interactive shell
 

links.ml
@@ -71,9 +71,9 @@ let rec directives
         match args with
           | [filename] ->
               let library_types, libraries =
-                (Errors.display_fatal Loader.read_file_cache (Settings.get_value prelude_file)) in 
+                (Errors.display_fatal Loader.load_file Library.typing_env (Settings.get_value prelude_file)) in 
               let libraries, _ = Interpreter.run_program [] [] libraries in
-              let sugar, pos_context = Parse.parse_file ~pp:(Settings.get_value pp) Parse.program filename in
+              let sugar, pos_context = Parse.parse_file Parse.program filename in
               let (bindings, expr), _, _ = Frontend.Pipeline.program Library.typing_env pos_context sugar in
               let defs = Sugar.desugar_definitions bindings in
               let expr = opt_map Sugar.desugar_expression expr in
@@ -200,7 +200,7 @@ let interact envs =
 let run_file prelude envs filename =
   Settings.set_value interacting false;
   let parse_and_desugar tyenv filename = 
-    let sugar, pos_context = Parse.parse_file ~pp:(Settings.get_value pp) Parse.program filename in
+    let sugar, pos_context = Parse.parse_file Parse.program filename in
     let (bindings, expr) as program, _, _ = Frontend.Pipeline.program tyenv pos_context sugar in
     let program = Sugar.desugar_program program in
       program, (bindings, expr)
@@ -222,6 +222,36 @@ let evaluate_string_in envs v =
 
 let to_evaluate : string list ref = ref []
 
+
+let load_prelude() = 
+  let prelude_types, (Syntax.Program (prelude_syntax, _) as prelude_program) =
+    (Errors.display_fatal
+       Loader.load_file Library.typing_env (Settings.get_value prelude_file))
+  in
+
+  let () = Library.prelude_env := Some prelude_types in
+
+  (* Having loaded a file, we now bump the internal type-variable
+     counter to ensure that type variables generated in the future
+     don't collide.
+     (BUG: we should really do something similar for term variables.)
+  *)
+  let _ =
+    let max_or (set : Types.TypeVarSet.t) (m : int) = 
+      if Types.TypeVarSet.is_empty set then
+        m
+      else max m (Types.TypeVarSet.max_elt set) in
+    let max_prelude_tvar =
+      List.fold_right max_or 
+        (Env.String.range (Env.String.map Types.free_bound_type_vars (prelude_types.Types.var_env)))
+        (Types.TypeVarSet.max_elt (Syntax.free_bound_type_vars_program prelude_program)) in
+      Types.bump_variable_counter max_prelude_tvar
+  in
+  let prelude_compiled = Interpreter.run_defs [] [] prelude_syntax in
+  let prelude_envs = 
+    (prelude_compiled, Types.extend_typing_environment Library.typing_env prelude_types) in
+    prelude_syntax, prelude_envs
+
 let run_tests tests () = 
   begin
     Test.run tests;
@@ -240,7 +270,14 @@ let options : opt list =
     ('n',     "no-types",            set printing_types false,         None);
     ('e',     "evaluate",            None,                             Some (fun str -> push_back str to_evaluate));
     (noshort, "config",              None,                             Some (fun name -> config_file := Some name));
-    (noshort, "dump",                None,                             Some Loader.dump_cached);
+    (noshort, "dump",                None,
+     Some(fun filename -> Loader.print_cache filename;  
+            Settings.set_value interacting false));
+    (noshort, "precompile",          None,
+     Some (fun filename ->
+             let _, (_, prelude_tyenv) = load_prelude() in
+             Loader.precompile_cache prelude_tyenv filename;
+               Settings.set_value interacting false));
     (noshort, "test",                Some (fun _ -> SqlcompileTest.test(); exit 0),     None);
     (noshort, "working-tests",       Some (run_tests Tests.working_tests),                  None);
     (noshort, "broken-tests",        Some (run_tests Tests.broken_tests),                   None);
@@ -255,40 +292,19 @@ let main () =
   end;
   config_file := (try Some (Unix.getenv "LINKS_CONFIG") with _ -> !config_file);
   let file_list = ref [] in
-  Errors.display_fatal_l (lazy (parse_cmdline options (fun i -> push_back i file_list)));
+  Errors.display_fatal_l (lazy 
+     (parse_cmdline options (fun i -> push_back i file_list)));
   (match !config_file with None -> () 
      | Some file -> Settings.load_file file);
-  (* load prelude: *)
-  let prelude_types, (Syntax.Program (prelude, _) as prelude_program) =
-    (Errors.display_fatal Loader.read_file_cache (Settings.get_value prelude_file)) in
 
-  let () = Library.prelude_env := Some prelude_types in
+  let prelude_syntax, prelude_envs = load_prelude() in
 
-  (* make sure the fresh type variable counter does not clash with any
-     type variables from the prelude
-
-     BUG: we should really do something similar for term variables
-  *)
-  let _ =
-    let max_or (set : Types.TypeVarSet.t) (m : int) = 
-      if Types.TypeVarSet.is_empty set then
-        m
-      else max m (Types.TypeVarSet.max_elt set) in
-    let max_prelude_tvar =
-      List.fold_right max_or 
-        (Env.String.range (Env.String.map Types.free_bound_type_vars (prelude_types.Types.var_env)))
-        (Types.TypeVarSet.max_elt (Syntax.free_bound_type_vars_program prelude_program)) in
-      Types.bump_variable_counter max_prelude_tvar
-  in
-  let prelude_compiled = Interpreter.run_defs [] [] prelude in
-  let stdenvs = 
-    (prelude_compiled, Types.extend_typing_environment Library.typing_env prelude_types) in
-  let () = Utility.for_each !to_evaluate (evaluate_string_in stdenvs) in
+  let () = Utility.for_each !to_evaluate (evaluate_string_in prelude_envs) in
     (* TBD: accumulate type/value environment so that "interact" has access *)
-  let () = Utility.for_each !file_list (run_file prelude stdenvs) in
-    if Settings.get_value(interacting) then
+  let () = Utility.for_each !file_list (run_file prelude_syntax prelude_envs) in
+    if Settings.get_value interacting then
       let () = print_endline (Settings.get_value welcome_note) in
-        interact stdenvs
+        interact prelude_envs
 
 let _ = 
   main ()

loader.ml
@@ -3,7 +3,8 @@ open Performance
 
 let `T (pos, dt, _) = Syntax.no_expr_data
 
-let identity x = x
+let use_cache = true
+let make_cache = true
 
 let expunge_source_pos =
   (Syntax.Functor_expression'.map
@@ -12,44 +13,90 @@ let expunge_source_pos =
 let expunge_all_source_pos =
   Syntax.transform_program expunge_source_pos
 
-let newer f1 f2 = 
-   ((Unix.stat f1).Unix.st_mtime > (Unix.stat f2).Unix.st_mtime) 
+let write_program (filename : string)
+    (tenv : Types.typing_environment) (program : Syntax.program) : unit =
+  call_with_open_outfile filename ~binary:true
+    (fun file ->
+       Marshal.to_channel file 
+         (* Serialise the typing environment returned from
+            sugar typing (which includes alias bindings), not
+            the typing environment returned from
+            Inference.type_program (which doesn't) *)
+         (tenv, expunge_all_source_pos program
+            : Types.typing_environment * Syntax.program)
+         [])
   
-let make_cache = true
+let read_file_source (filename:string) tyenv =
+  let sugar, pos_context = 
+      lazy (Parse.parse_file Parse.program filename) <|measure_as|> "parse" in
+  let program, _, tenv = 
+      Frontend.Pipeline.program tyenv pos_context sugar in
+  let program = Sugar.desugar_program program in
+  let env, program = 
+    lazy (Inference.type_program tyenv program) <|measure_as|> "type" in
+  let program = lazy (Optimiser.optimise_program (env, program)) 
+                <|measure_as|> "optimise" in
+  let program = Syntax.labelize program in
+    tenv, env, program
 
-let read_file_cache : string -> (Types.typing_environment * Syntax.program) = fun filename ->
-  let cachename = filename ^ ".cache" in
+let unmarshal_cache_file cachefile = 
+  (Marshal.from_channel cachefile : (Types.typing_environment * Syntax.program))
+
+let read_cache_file cachename = 
+  call_with_open_infile cachename ~binary:true unmarshal_cache_file
+
+let cache_directory_setting = 
+  Settings.add_string ("cache_directory", "", `User)
+
+let cachefile_path filename = 
+  let cachedir = Settings.get_value cache_directory_setting in
+    match cachedir with
+      | "" -> filename  ^ ".cache" (* Use current dir, no fancy filename  *)
+      | cachedir ->                (* Use given dir, put hash in filename *)
+          let path_hash = base64encode(Digest.string (absolute_path filename)) in
+          let cache_filename = (Filename.basename filename) ^ "-" ^ 
+                                 path_hash ^ ".cache" in
+            Filename.concat cachedir cache_filename
+    
+exception No_cache
+
+(** Load a file, considering and maintaining the cache. Includes 
+    parsing, desugaring, typechecking, optimising and labelizing. *)
+let load_file : _ -> string -> (Types.typing_environment * Syntax.program) = 
+  fun env infile ->
+  let cachename = cachefile_path infile in
+  let result = 
     try
-      if make_cache && newer cachename filename then
-        call_with_open_infile cachename ~binary:true
-          (fun cachefile ->
-             (Marshal.from_channel cachefile 
-                : (Types.typing_environment * Syntax.program)))
+      if not use_cache then None else
+      if getuid_owns cachename && newer cachename infile then
+        Some (read_cache_file cachename)
       else
-        (Debug.print("No precompiled " ^ filename);
-         raise (Sys_error "Precompiled source file out of date."))
-    with (Sys_error _| Unix.Unix_error _) ->
-      let sugar, pos_context = measure "parse" (Parse.parse_file ~pp:(Settings.get_value Basicsettings.pp) Parse.program) filename in
-      let program, _, tenv = Frontend.Pipeline.program Library.typing_env pos_context sugar in
-      let program = Sugar.desugar_program program in
-      let env, program = measure "type" (Inference.type_program Library.typing_env) program in
-      let program = measure "optimise" Optimiser.optimise_program (env, program) in
-      let program = Syntax.labelize program
-      in 
-	(try (* try to write to the cache *)
-           call_with_open_outfile cachename ~binary:true
-             (fun cachefile ->
-                Marshal.to_channel cachefile 
-                  (* Serialise the typing environment returned from
-                     sugar typing (which includes alias bindings), not
-                     the typing environment returned from
-                     Inference.type_program (which doesn't) *)
-                  (tenv, expunge_all_source_pos program
-                     : Types.typing_environment * Syntax.program)
-                  [])
-	 with _ -> ()) (* Ignore errors writing the cache file *);
-        tenv, program
-  
-let dump_cached filename =
-   let _env, program = read_file_cache filename in
+        raise No_cache
+    with (No_cache | Sys_error _| Unix.Unix_error _) ->
+      Debug.print("No valid cache for " ^ infile);
+      None
+  in
+    match result with
+        Some (tenv, program) -> tenv, program
+      | None -> 
+          (* Read & process the source *)
+          let tenv, env, program = read_file_source infile env in
+            if make_cache then
+            (try  (* to write to the cache *)
+               write_program cachename tenv program
+             with _ -> ()) (* Ignore errors writing the cache file *);
+            tenv, program
+
+(** Loads a named file and prints it as syntax; may use the cache or
+    the original file, as per the caching policy. *)
+let print_cache filename =
+   let _tenv, program = read_cache_file filename in
      print_string (Syntax.labelled_string_of_program program)
+
+let precompile env infile outfile : unit =
+  let tenv, env, program = read_file_source infile env in
+    write_program outfile tenv program
+
+let precompile_cache env infile : unit =
+  let outfile = infile ^ ".cache" in
+    precompile env infile outfile

loader.mli
@@ -1,0 +1,4 @@
+val load_file : Types.typing_environment -> string -> (Types.typing_environment * Syntax.program)
+val print_cache : string -> unit
+val precompile : Types.typing_environment -> string -> string -> unit
+val precompile_cache : Types.typing_environment -> string -> unit

settings.ml
@@ -135,8 +135,8 @@ let print_settings () =
                            match mode, is_user setting with
                              | `User, true
                              | `System, false ->
-			         (format_universal
-				    (Printf.sprintf " %-25s %-7s") setting)::p
+		                 (format_universal
+		                    (Printf.sprintf " %-25s %-7s") setting)::p
                              | _ -> p) !settings []) in
   let user_settings =
     ("User settings" :: get_settings `User) in

test.ml
@@ -35,7 +35,7 @@ let parse = attempt parse_thingy
     
 
 let optimise = attempt (fun program -> Optimiser.optimise_program (Library.typing_env, program))
-let run tests = attempt (let _, prelude = Loader.read_file_cache (Settings.get_value Basicsettings.prelude_file) in
+let run tests = attempt (let _, prelude = Loader.load_file Library.typing_env (Settings.get_value Basicsettings.prelude_file) in
                          let prelude, _ = Interpreter.run_program [] [] prelude in
                            Interpreter.run_program prelude [] ->- snd) tests
 let show = attempt Result.string_of_result

commit 3bda977d998b8230b4ec75daf6a4937eff6a7a2e

Fix a bug (introduced in r1554) where the preprocessor was always being ignored.
(Settings should not be read until the config file has been
processed!)

parse.ml
@@ -100,7 +100,7 @@ let normalize_context = function
   | None -> fresh_context ()
   | Some c -> c
 
-let default_preprocessor = (Settings.get_value Basicsettings.pp) 
+let default_preprocessor () = (Settings.get_value Basicsettings.pp) 
 
 (** Public functions: parse some data source containing Links source
     code and return a list of ASTs. 
@@ -110,7 +110,7 @@ let default_preprocessor = (Settings.get_value Basicsettings.pp)
     intercept and retain the code that has been read (in order to give
     better error messages).
 **)
-let parse_string ?(pp=default_preprocessor) ?in_context:context grammar string =
+let parse_string ?(pp=default_preprocessor ()) ?in_context:context grammar string =
   let pp = normalize_pp pp
   and context = normalize_context context in 
     read ?nlhook:None ~parse:grammar ~infun:(reader_of_string ?pp string) ~name:"<string>" ~context
@@ -119,7 +119,7 @@ let parse_channel ?interactive ?in_context:context grammar (channel, name) =
   let context = normalize_context context in
     read ?nlhook:interactive ~parse:grammar ~infun:(reader_of_channel channel) ~name:name ~context
 
-let parse_file ?(pp=default_preprocessor) ?in_context:context grammar filename =
+let parse_file ?(pp=default_preprocessor ()) ?in_context:context grammar filename =
   match normalize_pp pp with
     | None -> parse_channel ?in_context:context grammar (open_in filename, filename)
     | Some pp ->