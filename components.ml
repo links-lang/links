e2ae6f416c195fbda2a7028caf7827e83a68677d
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2010-10-27 14:11:01 +0000

    removed redundant type annotations.

--- components.ml
+++ components.ml
@@ -33,15 +33,15 @@ struct
   type value = AList.value
   type t = (AList.key * AList.value) list
   let empty = []
-  let keys (al : t) = List.map fst al
-  let columns (al : t) = List.map (AList.col -<- fst) al
-  let incr_cols (al : t) i = List.map (fun (key, value) -> (AList.incr key i, value)) al
-  let decr_cols (al : t) i = List.map (fun (key, value) -> (AList.decr key i, value)) al
-  let append (l : t) (r : t) = List.append l r
-  let lookup (al : t) (k : key) = List.assoc k al
-  let length (l : t) = List.length l
-  let lookup_col (al : t) c = List.filter (fun (key, _) -> (AList.col key) = c) al
-  let keep_cols (al : t) cols = List.filter (fun (key, _) -> List.mem (AList.col key) cols) al
+  let keys al = List.map fst al
+  let columns al = List.map (AList.col -<- fst) al
+  let incr_cols al i = List.map (fun (key, value) -> (AList.incr key i, value)) al
+  let decr_cols al i = List.map (fun (key, value) -> (AList.decr key i, value)) al
+  let append l r = List.append l r
+  let lookup al k = List.assoc k al
+  let length l = List.length l
+  let lookup_col al c = List.filter (fun (key, _) -> (AList.col key) = c) al
+  let keep_cols al cols = List.filter (fun (key, _) -> List.mem (AList.col key) cols) al
 end
 
 module Default =
