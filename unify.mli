fe69ca2aa41992d2741b60ad4d2a2b168e4f5fdc
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2011-02-23 16:56:39 +0000

    expose type/row etc comparison functions in unify.mli. shouldn't those be moved to types.ml/typeUtils.ml?

--- unify.mli
+++ unify.mli
@@ -7,3 +7,11 @@ exception Failure of error
 
 val datatypes : Types.datatype * Types.datatype -> unit
 val rows : Types.row * Types.row -> unit
+
+(* FIXME: move to types.ml/typeUtils.ml *)
+val eq_types : Types.datatype * Types.datatype -> bool
+val eq_quantifier : Types.quantifier * Types.quantifier -> bool
+val eq_rows : Types.row * Types.row -> bool
+val eq_presence : Types.presence_flag * Types.presence_flag -> bool
+val eq_row_vars : Types.row_var * Types.row_var -> bool
+val eq_type_args : Types.type_arg * Types.type_arg -> bool
