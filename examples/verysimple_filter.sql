9c9f32af2faccdcb80a452f9882d3375b11ba826
Gabriel Radanne <gabriel.radanne@zoho.com>
2013-05-23 19:18:35 +0000

    An exemple of database for the verysimple_filter.

---
+++ examples/verysimple_filter.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ab_store (
+	 name text,
+    a integer,
+    b integer
+);
+
+INSERT INTO ab_store VALUES ('julie',2,3);
+INSERT INTO ab_store VALUES ('benoit',10,8);
+INSERT INTO ab_store VALUES ('gabriel',20,20);
+INSERT INTO ab_store VALUES ('clement',1,15);
+INSERT INTO ab_store VALUES ('sophie',12,3);
