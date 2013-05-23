f02cb4202e1b1a3a43549c6cbfb9f0e291150a15
Gabriel Radanne <gabriel.radanne@zoho.com>
2013-05-23 19:12:54 +0000

    Add some benchmarks and exemples. Use ocamlfind to get posgresql path.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 ./links --debug -c $1 > /tmp/tmp.ml
-ocamlopt -annot -I /opt/local/lib/ocaml/site-lib/postgresql/ -I lib/compiler -ccopt -L/opt/local/lib/ocaml/site-lib/postgresql/ -thread bigarray.cmxa unix.cmxa threads.cmxa nums.cmxa str.cmxa deriving-310/lib/deriving.cmxa postgresql.cmxa links.cmxa /tmp/tmp.ml -o $2
-
+ocamlfind ocamlopt \
+	-I lib/compiler \
+	-thread bigarray.cmxa unix.cmxa  \
+	threads.cmxa nums.cmxa str.cmxa \
+	deriving-310/lib/deriving.cmxa \
+	-package postgresql postgresql.cmxa links.cmxa \
+	/tmp/tmp.ml \
+	-o $2
