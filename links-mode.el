c68ece34ba98041f0510578c0c3b86d5861a742c
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-11-18 17:42:40 +0000

    Rename current and remove old emacs mode.

--- links-mode.el
+++ links-mode.el
@@ -1,129 +1,84 @@
-;; Mode for editing Links files.
-;; Will eventually provide at least:
-;;   * syntax highlighting
-;;   * indentation
-;;   * interpreter integration (send commands to interpreter, jump to
-;;     error in source code buffer)
-;;   * block navigation
-;; and perhaps
-;;   * context-sensitive completion (ala Slime, nxml-mode)
-;;   * type checking as you type (ala Java mode in Eclipse)
-;;   * context-sensitive help
-;;   * jump to definition (M-.)
-;;   * other stuff
-
-(defun links-mode ()
-  "Major mode for editing Links files
-
-Key bindings
-\\{links-mode-map}
-"	; TODO: expand this docstring
-  (interactive)
-  (kill-all-local-variables)
-  (setq major-mode 'links-mode
- 	mode-name "Links"
- 	indent-line-function 'links-indent-line)
-    (make-local-variable 'font-lock-defaults)
-  (setq font-lock-defaults '(links-font-lock-keywords t))
-  ;;(set (make-local-variable 'imenu-generic-expression) 'links-mode-imenu-thing)
-  (set-syntax-table links-mode-syntax-table)
-  (use-local-map links-mode-map)
-  
-  ;;   ;; last thing to do
-  (run-hooks 'links-mode-hook))
+;;; links-mode.el -- A first gasp at an Emacs mode for Links
+;;;                  (Horribly broken, for sure.)
+;;; by Ezra Cooper 2007
+;;; Many bits copped from caml.el by Leroy et al.
+;;;
+;;; Things that work:
+;;;    * some syntax highlighting
+;;;    * (un)comment-region
+;;;
+;;; Things that don't work:
+;;;    * syntax highlighting of XML quasis.
+;;;
+;;; Report problems to e.e.k.cooper@sms.ed.ac.uk
 
 
-(defun links-indent-line ())
-
-(defun links-internal-fontlock-set (face &rest disjuncts)
-  (cons (format "\\<\\(%s\\)\\>" (regexp-opt disjuncts)) face))
-
-(defvar links-font-lock-keywords
-  (list 
-   ;; builtin functions/operators
-   (links-internal-fontlock-set 'font-lock-function-name-face
-				"string_of_bool" "float_of_int" "float_of_string" "int_of_string"
-				"bool_of_string" "string_of_int" "string_of_float"
-				"pre" "abs")
-   ;; keywords
-   (links-internal-fontlock-set 'font-lock-builtin-face
-				"exit" "def" "defrec" "let" "letrec" "in" "and"
-				"fun" "if" "then" "else" "case" "of" "or" "for"
-				"sort_up" "sort_down" "table" "from" "with"
-				"unique" "order" "asc" "desc" "database")
-   ;; constants
-   (links-internal-fontlock-set 'font-lock-constant-face
-				"true" "false")
-
-   ;; types
-   (links-internal-fontlock-set 'font-lock-type-face
-				"bool" "int" "float" "string")))
-   
-(defvar links-mode-font-lock-thing 
-  '(links-font-lock-keywords 		; keywords
-    nil 				; keywords-only
-    nil					; case-fold
-    nil                       	       	; syntax-alist
-    nil)				; syntax-begin
- "Links mode highlighting information")
-
-(defvar links-mode-map
-  (let ((links-mode-map (make-keymap)))
-    (define-key links-mode-map "\C-j" 'newline-and-indent)
-    (define-key links-mode-map "\C-c\C-c" 'links-evaluate-region)
-    links-mode-map))
-
-
-
-
-(defvar links-mode-syntax-table 
-  (let ((links-mode-syntax-table (make-syntax-table)))
-    (modify-syntax-entry ?_ "w" links-mode-syntax-table)
-    (modify-syntax-entry ?( "(" links-mode-syntax-table)
-    (modify-syntax-entry ?{ "(" links-mode-syntax-table)
-    (modify-syntax-entry ?[ "(" links-mode-syntax-table)
-    (modify-syntax-entry ?< "(" links-mode-syntax-table)
-    (modify-syntax-entry ?) ")" links-mode-syntax-table)
-    (modify-syntax-entry ?} ")" links-mode-syntax-table)
-    (modify-syntax-entry ?] ")" links-mode-syntax-table)
-    (modify-syntax-entry ?> ")" links-mode-syntax-table)
-
-    links-mode-syntax-table))
-
-(defvar links-mode-abbrev-table nil "Links mode abbrev table")
-(defvar links-mode-imenu-thing nil "Links mode imenu information")
-(defvar links-mode-hook nil "Links mode hook")
-
-
-(defvar links-program-name "/home/s0456219/code/links/gilles/2005-01-10/code 0.6.5 (no-opt)/slinks")
-
-(defun run-links ()
-  "Run an inferior Links process, input and output via buffer *slinks*."
-  (interactive)
-  (switch-to-buffer-other-window (make-comint "slinks" links-program-name nil "-i"))
+(defvar links-mode-syntax-table nil
+  "Syntax table for links-mode buffers")
+(if links-mode-syntax-table
+    ()
+  (setq links-mode-syntax-table (make-syntax-table))
+  (modify-syntax-entry ?\\ "\\" links-mode-syntax-table)
+  (modify-syntax-entry ?# "<" links-mode-syntax-table)
+  (modify-syntax-entry ?\n ">" links-mode-syntax-table)
+  (modify-syntax-entry ?{ "(}" links-mode-syntax-table)
+  (modify-syntax-entry ?} "){" links-mode-syntax-table)
+  (modify-syntax-entry ?_ "w" links-mode-syntax-table)
   )
 
-(defun inferior-links-mode ()
-  "Major mode for interacting with the Links interpreter"
-  (interactive)
-  (require 'comint)
-  (comint-mode)
-  (setq comint-prompt-regexp "^ *? *")
-  (use-local-map links-mode-map)
-  (run-hooks 'links-mode-hook))
+(defconst links-font-lock-keywords
+  (list
+   ; comments
+   '("\\(^\\|[^</]\\)#.*$" . font-lock-comment-face)
+   ; XML forests
+   '("<#>.*</#>" . font-lock-xml-face)
+   ; XML tags
+   '("</?[a-z][^>]*>" 0 font-lock-xml-face t)
+   ; XML escapes (attributes)
+   '("\"{[^}]*}\"" 0 font-lock-normal-face t)
+   ; declarations
+   '("\\<fun\\|var\\|sig\\>" . font-lock-type-face)
+   ; special operations
+   '("\\<\\(table\\|with\\|from\\|for\\|where\\|switch\\|case\\|database\\|if\\|else\\|insert\\|values\\|form\\|yields\\)\\>" 
+     . font-lock-keyword-face)
+   ; types & variant tags
+   '("\\<[A-Z][A-Za-z0-9_]*\\>" . font-lock-function-name-face)
+   ; variable names
+   '("\\<\\(fun\\|var\\|sig\\) +\\([a-z][A-Za-z0-9_]*\\)\\>" 
+     2 font-lock-variable-name-face)
+   ; variable names
+   '("\\<\\(l:name\\)=\"\\([a-z][A-Za-z0-9_]*\\)\"" 
+     2 font-lock-variable-name-face t)
+   ; type operators
+   '("->" . font-lock-function-name-face)
+   ))
 
-(defun links-evaluate-region ()
+(defun initialize-font-lock-defaults ()
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults
+        '(links-font-lock-keywords nil nil ((?' . "w") (?_ . "w")) nil))
+  ; TBD: the last argument is SYNTAX-BEGIN, for determining the
+  ; beginning of a syntactic feature.
+  )
+
+(defun links-mode ()
   (interactive)
-  (let ((beg nil)
-	(end nil))
-    (if mark-active
-	(setq beg (region-beginning)
-	      end (region-end))
-      (setq beg (line-beginning-position)
-	    end (line-end-position)))
-    (save-excursion
-      (send-region "slinks" beg end)
-      (send-string "slinks" ";;\n")
-      (display-buffer "*slinks*"))))
+  (kill-all-local-variables)
+
+  (setq major-mode 'links-mode)
+  (setq mode-name "Links")
+
+  (make-local-variable 'require-final-newline)
+  (setq require-final-newline t)
+  (make-local-variable 'comment-start)
+  (setq comment-start "#")
+
+  (make-local-variable 'comment-start-skip)
+  (setq comment-start-skip "#+ *")
+  
+  (initialize-font-lock-defaults)
+
+  (run-hooks 'links-mode-hook)
+  )
 
 (provide 'links-mode)
