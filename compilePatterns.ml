092bebbcfe73e52d83cc3981ec60020aae830af2
dhil <s1467124@sms.ed.ac.uk>
2015-07-03 19:16:03 +0100

    Fixed handler case pattern matching bug on variants. Todo: Clean up the code.

--- compilePatterns.ml
+++ compilePatterns.ml
@@ -878,7 +878,22 @@ let rec match_handle_cases : var -> clause list -> (Types.datatype * Types.row) 
 				  let computation =
 				    let Some p = StringMap.lookup "1" fields in
 				    let (nenv,tenv,eff,_) = env in
-				    let_pattern (nenv,tenv,eff) p (`Project ("1", `Variable y), optype) (body env, output_type)
+				    let t =
+				      match optype with
+					  `Record (fields,row_var,dual) -> 
+					  let Some fields = StringMap.lookup "1" fields in
+					  begin
+					    match fields with
+					      `Present p ->
+					      begin
+						match p with
+						  `Record (fields,_,_) -> `Record (fields,row_var,dual)
+						 | _ -> assert false
+					      end
+					    | _ -> assert false
+					  end
+					| _ -> assert false in
+				    let_pattern (nenv,tenv,eff) p (`Project ("1", `Variable y), t) (body env, output_type)
 				  in
 				  let continuation_binder =
 				    let Some k = StringMap.lookup "2" fields in
