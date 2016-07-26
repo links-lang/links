b94b1f961f97b2087a7b3630771b0dad8ff316a0
Simon Fowler <simon@simonjf.com>
2016-07-26 16:55:37 +0100

    Fix scoping issues in module renaming
    Now we should have correct scoping, without the problems with ambiguity
    that plagued the previous implementation, hurrah!
    
    Also, the desugaring pass is now only performed if there are modules /
    imports. No point otherwise.

--- desugarModules.ml
+++ desugarModules.ml
@@ -30,6 +30,15 @@ open Printf
 
 let module_sep = ":::"
 
+(* Renaming stack. We either have a variable binding or an "open" statement.
+ * If we have a variable binding after an "open" statement, then this should
+ * take priority over the variables contained within the opened module.
+ * Stack frames persist for a single scope (i.e. `Block) *)
+type binding_stack_node = [
+  | `OpenStatement of string
+  | `LocalVarBinding of string
+]
+
 (* Only `Module and `Import don't preserve structure. We can just do a map on everything
  * else... *)
 let rec flatten_simple = fun () ->
@@ -86,16 +95,21 @@ let print_list xs =
   "[" ^ print_list_inner xs ^ "]"
 
 
-let prefixWith name prefix =
-  if prefix = "" then name else prefix ^ module_sep ^ name
+let print_stack_node = function
+  | `OpenStatement mn -> "module: " ^ mn
+  | `LocalVarBinding lvb -> "var: " ^ lvb
+
+let print_stack s = print_list (List.map print_stack_node s)
+let prefixWith name prefix = if prefix = "" then name else prefix ^ module_sep ^ name
 
 (* Given a plain module name, checks whether it is in scope according to the
  * current stack of open modules *)
-let rec moduleInScope seen_modules module_scope_stack module_name =
-  match module_scope_stack with
+let rec moduleInScope seen_modules binding_stack module_name =
+  match binding_stack with
     | [] ->
         if StringSet.mem module_name seen_modules then Some(module_name) else None
-    | x::xs ->
+    | (`LocalVarBinding _)::xs -> moduleInScope seen_modules xs module_name
+    | (`OpenStatement x)::xs ->
         let fully_qual = prefixWith module_name x in
         if StringSet.mem fully_qual seen_modules then
           Some(fully_qual)
@@ -105,10 +119,21 @@ let rec moduleInScope seen_modules module_scope_stack module_name =
 (* Given a module stack, a reference tree, and a plain variable name, resolves
  * the fully-qualified name according to the priority of the stack.
  * If there are no matches, leave it as it is (either FQ already or erroneous) *)
-let rec substituteVar seen_bindings module_scope_stack current_module_prefix var_name =
-  match module_scope_stack with
+let rec substituteVar seen_bindings binding_stack current_module_prefix var_name =
+  (*
+  printf "Trying to substitute for %s in module %s. Seen: %s; Binding stack: %s\n"
+    var_name current_module_prefix (print_list (StringSet.elements seen_bindings)) (print_stack binding_stack);
+ *)
+  (* printf "Trying to substitute for %s in module %s; Binding stack: %s\n"
+    var_name current_module_prefix (print_stack binding_stack); *)
+  match binding_stack with
     | [] -> var_name
-    | x::xs ->
+    | (`LocalVarBinding x)::xs ->
+        if x = var_name then
+          prefixWith x current_module_prefix
+        else
+          substituteVar seen_bindings xs current_module_prefix var_name
+    | (`OpenStatement x)::xs ->
         let fully_qual = prefixWith var_name x in
         if StringSet.mem fully_qual seen_bindings then
           fully_qual
@@ -118,33 +143,36 @@ let rec substituteVar seen_bindings module_scope_stack current_module_prefix var
 
 
 (* Add module prefix to all internal binder names and variables *)
-let rec add_module_prefix prefix init_seen_modules init_seen_bindings init_module_scope_stack =
+let rec add_module_prefix prefix init_seen_modules init_seen_bindings init_binding_stack =
 object(self)
   inherit SugarTraversals.fold_map as super
 
   val seen_modules = init_seen_modules
   val seen_bindings = init_seen_bindings
-  val module_scope_stack = init_module_scope_stack
+  val binding_stack = init_binding_stack
 
   method add_seen_module name =
     {< seen_modules = StringSet.add name seen_modules >}
 
   method push_module name =
-    {< module_scope_stack = name :: module_scope_stack >}
+    {< binding_stack = (`OpenStatement name) :: binding_stack >}
+
+  method push_var_binding name =
+    {< binding_stack = (`LocalVarBinding name) :: binding_stack >}
 
   method add_seen_binding name =
     {< seen_bindings = StringSet.add name seen_bindings >}
 
-  method get_module_stack = module_scope_stack
+  method get_binding_stack = binding_stack
   method get_seen_modules = seen_modules
   method get_seen_bindings = seen_bindings
 
-  method set_stack s = {< module_scope_stack = s >}
+  method set_stack s = {< binding_stack = s >}
 
   method phrase = function
     | (`Var old_name, pos) ->
         (* Add prefix onto var name*)
-        let new_name = substituteVar seen_bindings module_scope_stack prefix old_name in
+        let new_name = substituteVar seen_bindings binding_stack prefix old_name in
         (self, (`Var new_name, pos))
     | x -> super#phrase x
 
@@ -152,40 +180,48 @@ object(self)
   method binder = function
     | (old_name, dt, pos) ->
         let new_name = prefixWith old_name prefix in
-        (self#add_seen_binding new_name, (new_name, dt, pos))
+        let o = (self#push_var_binding old_name) in
+        (o#add_seen_binding new_name, (new_name, dt, pos))
 
   method bindingnode = function
     | `Import name ->
         (* Check to see whether module is in our seen list. If so, we're golden,
          * push onto open module stack (fully-qualified).
          * If not, throw an error. *)
-        (match moduleInScope seen_modules module_scope_stack name with
+        (match moduleInScope seen_modules binding_stack name with
            | Some(fully_qualified_name) ->
-               (self#push_module fully_qualified_name, `Import name)
+               (self#push_module fully_qualified_name, `Import fully_qualified_name)
            | None ->
                failwith ("Trying to import unknown module " ^ name))
-    | `Module (name, (`Block (bindings, dummy_phrase), pos)) ->
+    | `Module (name, p) ->
         let new_prefix = prefixWith name prefix in
         (* Add fully-qualified module to seen module list *)
         (* Add fully-qualified module to module stack *)
-        let o = (add_module_prefix new_prefix (StringSet.add new_prefix seen_modules)
-          seen_bindings (new_prefix :: module_scope_stack)) in
+        let o = self#add_seen_module new_prefix in
+        let o1 = o#push_module new_prefix in
+        let (o2, renamed_phrase) =
+          (add_module_prefix new_prefix o1#get_seen_modules o1#get_seen_bindings
+            ((`OpenStatement new_prefix)::binding_stack))#phrase p in
+        let o3 = {< seen_modules = o2#get_seen_modules; seen_bindings = o2#get_seen_bindings >} in
+        (o3, `Module (new_prefix, renamed_phrase))
+        (* printf "Renamed bindings for module %s: %s\n" name (print_list
+         * (List.map (Sugartypes.Show_binding.show)  (List.rev reversed_renamed_bindings))); *)
+    | x -> super#bindingnode x
+
+  method phrasenode = function
+    | `Block (bs, p) ->
+        (* Recursively process bindings / phrase *)
         let (o1, reversed_renamed_bindings) =
           List.fold_left (fun (o_acc, bs_acc) binding ->
             let (o_acc1, new_binding) = o_acc#binding binding in
             (o_acc1, new_binding :: bs_acc)
-          ) (o, []) bindings in
-        (* printf "Renamed bindings for module %s: %s\n" name (print_list
-         * (List.map (Sugartypes.Show_binding.show)  (List.rev reversed_renamed_bindings))); *)
-        let o2 = {< seen_modules = o1#get_seen_modules; seen_bindings = o1#get_seen_bindings >} in
-        (o2, `Module (new_prefix, (`Block (List.rev reversed_renamed_bindings, dummy_phrase), pos)))
-        (* let (o1, renamed_bindings) = o#list (fun o -> o#binding) bindings in *)
-        (* Restore old stack *)
-        (* printf "Renamed bindings for module %s: %s\n" name (print_list
-         * (List.map (Sugartypes.Show_binding.show)  (List.rev reversed_renamed_bindings))); *)
-        (* (o2, `Module (new_prefix, (`Block (renamed_bindings, dummy_phrase), pos))) *)
-    | x -> super#bindingnode x
-
+          ) (self, []) bs in
+        (* Restore the previous binding stack by making a copy of this object w/ new
+         * seen modules / bindings *)
+        let (o2, new_phrase) = o1#phrase p in
+        let o2 = {< seen_modules = o2#get_seen_modules; seen_bindings = o2#get_seen_bindings >} in
+        (o2, `Block (List.rev reversed_renamed_bindings, new_phrase))
+    | p -> super#phrasenode p
 
   method program = function
     | (bindings, body) ->
@@ -196,22 +232,40 @@ object(self)
 end
 
 let performRenaming prog =
-  snd ((add_module_prefix "" (StringSet.empty) (StringSet.empty) [""])#program prog)
+  snd ((add_module_prefix "" (StringSet.empty) (StringSet.empty) [`OpenStatement ""])#program prog)
+
+
+let has_no_modules =
+object
+  inherit SugarTraversals.predicate as super
+
+  val has_no_modules = true
+  method satisfied = has_no_modules
+
+  method bindingnode = function
+    | `Import _
+    | `Module _ -> {< has_no_modules = false >}
+    | b -> super#bindingnode b
+end
+
+let requires_desugar prog = (not ((has_no_modules#program prog)#satisfied))
 
 (* 1) Perform a renaming pass to expand names in modules to qualified names
  * 2) Peform a flattening pass to flatten modules to lists of bindings
  *)
 let desugar_modules program =
-  let renamedProgram = performRenaming program in
-  let (renamedBindings, renamedBody) = renamedProgram in
-  let flattenedProgram = performFlattening renamedProgram in
-  (*
-  printf "\n=============================================================\n";
-  printf "\n=============================================================\n";
-  printf "Before: %s\n" (Sugartypes.Show_program.show program);
-  printf "\n=============================================================\n";
-  printf "After: %s\n" (Sugartypes.Show_program.show flattenedProgram);
-  *)
-  flattenedProgram
+  if (requires_desugar program) then
+    let renamedProgram = performRenaming program in
+    let (renamedBindings, renamedBody) = renamedProgram in
+    let flattenedProgram = performFlattening renamedProgram in
+    (*
+    printf "\n=============================================================\n";
+    printf "\n=============================================================\n";
+    printf "Before: %s\n" (Sugartypes.Show_program.show program);
+    printf "\n=============================================================\n";
+    printf "After: %s\n" (Sugartypes.Show_program.show flattenedProgram);
+    *)
+    flattenedProgram
+  else program
 
 
