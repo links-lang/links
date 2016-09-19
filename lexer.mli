87dc1a6a5b62d8b8c93baf9eb7c8a79ca863c355
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-09-19 16:43:45 +0100

    New option --print-keywords to make Links print its language keywords (#65)
    ...and update Emacs mode accordingly.

--- lexer.mli
+++ lexer.mli
@@ -7,6 +7,9 @@ val fresh_context : unit -> lexer_context
 (** An unexpected character was encountered in the lexing buffer. *)
 exception LexicalError of (string * Lexing.position)
 
+(** Links language keywords *)
+val keywords : (string * Parser.token) list
+
 (** The entry point to the lexer. *)
 val lexer : lexer_context
          -> newline_hook:(unit -> unit) 
