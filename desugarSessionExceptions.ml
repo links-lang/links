ed33c044ad53898279dff017d09792db478bec02
Simon Fowler <simon@simonjf.com>
2017-10-12 22:07:07 +0100

    Top-level exception handlers for spawned processes.
    This fixes most of the test cases on the client.
    
    It seems to mess up cancel6 on the server though, so will need to
    investigate that further.

--- desugarSessionExceptions.ml
+++ desugarSessionExceptions.ml
@@ -36,11 +36,38 @@ module TyEnv = Env.String
 let failure_op_name = Value.session_exception_operation
 
 let dp = Sugartypes.dummy_position
+let mk_var_pat name : pattern = (`Variable (name, Some `Not_typed, dp), dp)
+let dummy_pat () = mk_var_pat @@ Utility.gensym ~prefix:"dsh" ()
+
+class insert_toplevel_handlers env =
+object (o: 'self_type)
+  inherit (TransformSugar.transform env) as super
+
+  method! phrasenode = function
+    | (`Spawn (`Wait, _, _, _)) as sw ->
+        super#phrasenode sw
+    | `Spawn (k, spawn_loc, (body, body_loc), Some inner_effects) ->
+        let as_var = Utility.gensym ~prefix:"spawn_aspat" () in
+        let as_pat = mk_var_pat as_var in
+        let unit_phr = (`RecordLit ([], None), dp) in
+
+        let (o, spawn_loc) = o#given_spawn_location spawn_loc in
+        let envs = o#backup_envs in
+        let (o, inner_effects) = o#row inner_effects in
+        let process_type = `Application (Types.process, [`Row inner_effects]) in
+        let o = o#with_effects inner_effects in
+        let (o, body, _) = o#phrasenode body in
+        let body =
+          `TryInOtherwise ((body, body_loc), as_pat, (`Var as_var, dp), unit_phr, (Some (Types.unit_type))) in
+        let o = o#restore_envs envs in
+        (o, (`Spawn (k, spawn_loc, (body, body_loc), Some inner_effects)), process_type)
+    | e -> super#phrasenode e
+end
+
 
 class desugar_session_exceptions env =
 object (o : 'self_type)
   inherit (TransformSugar.transform env) as super
-
 
   method! phrasenode = function
     | `Raise -> (o, `DoOperation (failure_op_name, [], Some `Not_typed), `Not_typed)
@@ -56,7 +83,6 @@ object (o : 'self_type)
         let (o, otherwise_phr, otherwise_dt) = o#phrase otherwise_phr in
         (* Now, to create a handler... *)
 
-        let mk_var_pat name : pattern = (`Variable (name, Some `Not_typed, dp), dp) in
         let return_pat = (`Variant ("Return", Some (pat)), dp) in
         let return_clause = (return_pat, as_phr) in
         (* Otherwise clause: Distinguished 'session failure' name. Since
@@ -65,7 +91,7 @@ object (o : 'self_type)
          * continuation argument. *)
         let return_captured_pat = mk_var_pat "_return_captured" in
         let cont_captured_pat = mk_var_pat "_cont_captured" in
-        let cont_pat = mk_var_pat @@ Utility.gensym ~prefix:"dsh" () in
+        let cont_pat = dummy_pat () in
 
 
         (* Bit of a hack on the otherwise front. We make a pair of binders.
@@ -111,6 +137,11 @@ object (o : 'self_type)
     | e -> super#phrasenode e
 end
 
+let insert_toplevel_handlers env =
+  ((new insert_toplevel_handlers env) :
+    insert_toplevel_handlers :> TransformSugar.transform)
+
 let desugar_session_exceptions env =
   ((new desugar_session_exceptions env) :
     desugar_session_exceptions :> TransformSugar.transform)
+
--- desugarSessionExceptions.mli
+++ desugarSessionExceptions.mli
@@ -1,1 +1,2 @@
+val insert_toplevel_handlers : Types.typing_environment -> TransformSugar.transform
 val desugar_session_exceptions : Types.typing_environment -> TransformSugar.transform
