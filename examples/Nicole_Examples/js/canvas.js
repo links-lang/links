7809ad9b3a3cb9cca87260acccf365ad71b063ed
Corneliusbusch <nicole.meng@hotmail.de>
2018-07-13 16:19:01 +0100

    border drawing

--- examples/Nicole_Examples/js/canvas.js
+++ examples/Nicole_Examples/js/canvas.js
@@ -15,6 +15,11 @@ function drawCustomUnit(x, y, xheight, yheight, color, ctx, kappa) {
 function getValueFromSelection(id, kappa){
     var e = document.getElementById(id);
     return _yieldCont(kappa, e.value);
-
 }
 
+function setBorderOfRef(node, style, kappa){
+    node.style.border = style;
+    return _yieldCont(kappa, node);
+}
+
+CONSTANTS.UNIT
