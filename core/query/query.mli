76f4b6d6c15577ba3f21e12bba6da243dfc8456a
wricciot <research@wilmer-ricciotti.net>
2019-01-28 13:05:25 +0100

    Cleans some unnecessary double semi-colons.

--- core/query/query.mli
+++ core/query/query.mli
@@ -1,4 +1,4 @@
-open Utility;;
+open Utility
 
 type tag = int
   [@@deriving show]
@@ -20,26 +20,23 @@ and env = Value.env * t Env.Int.t
   [@@deriving show]
 
 val unbox_xml : [> `XML of 'a ] -> 'a
-    ;;
+
 val unbox_pair : [> `Record of 'a Utility.StringMap.t ] -> 'a * 'a
-    ;;
+
 val unbox_list : ([> `Concat of 'a list | `Singleton of 'b ] as 'a) -> 'b list
-    ;;
+
 val unbox_string : [> `Concat of [> `Concat of 'a
                                   | `Singleton of [> `Constant of [> `Char of char ] ] as 'b ]
                 list as 'a
             | `Constant of [> `String of string ]
             | `Singleton of 'b ] ->
 		string
-;;
 
 val used_database : t -> Value.database option
-    ;;
 
 val string_of_t : t -> string
 
 val type_of_expression : t -> Types.datatype
-    ;;
 
 val default_of_base_type : [> `Bool | `Char | `Float | `Int | `String ] ->
            [> `Constant of
@@ -48,7 +45,7 @@ val default_of_base_type : [> `Bool | `Char | `Float | `Int | `String ] ->
                  | `Float of float
                  | `Int of int
                  | `String of string ] ]
-;;
+
 val value_of_expression : ([> `Concat of [> `Singleton of 'a ] list
              | `Constant of
                  [> `Bool of bool
@@ -62,43 +59,23 @@ val value_of_expression : ([> `Concat of [> `Singleton of 'a ] list
              | `XML of Value.xmlitem ]
             as 'a) ->
            Value.t
-;;
 
-val labels_of_field_types : 'a Utility.StringMap.t -> Utility.StringSet.t;;
-val record_field_types : Types.datatype -> Types.datatype StringMap.t;;
-val table_field_types : Value.table -> Types.typ Utility.StringMap.t;;
+val labels_of_field_types : 'a Utility.StringMap.t -> Utility.StringSet.t
+val record_field_types : Types.datatype -> Types.datatype StringMap.t
+val table_field_types : Value.table -> Types.typ Utility.StringMap.t
 val is_list : [> `Concat of 'a
             | `For of 'b
             | `If of 'c * 'd * [> `Concat of 'e list ]
             | `Singleton of 'f
-            | `Table of 'g ] -> bool;;
+            | `Table of 'g ] -> bool
 
 module Eval :
 sig
-  (* val nil : unit *)
-  (* val eval_error : unit *)
   val env_of_value_env : 'a -> 'a * 'b Env.Int.t
-  (* val (++) : unit *)
-  (* val lookup_fun : unit *)
-  (* val find_fun : unit *)
-  (* val expression_of_value : unit *)
   val bind : 'a * 'b Env.Int.t -> Env.Int.name * 'b -> 'a * 'b Env.Int.t
-  (* val lookup : unit *)
   val eta_expand_var : Var.var * Types.datatype StringMap.t -> t
-  (* val eta_expand_list : unit *)
-  (* val value : unit *)
-  (* val apply : unit *)
   val computation : Value.t Value.Env.t * t Env.Int.t -> Ir.computation -> t
-  (* val tail_computation : unit *)
-  (* val reduce_concat : unit *)
-  (* val reduce_for_source : unit *)
-  (* val reduce_for_body : unit *)
-  (* val reduce_if_condition : unit *)
   val reduce_where_then : t * t -> t
-  (* val reduce_if_body : unit *)
   val reduce_and : t * t -> t
-  (* val reduce_or : unit *)
-  (* val reduce_not : unit *)
-  (* val reduce_eq : unit *)
   val eval : Value.t Value.Env.t -> Ir.computation -> t
 end
