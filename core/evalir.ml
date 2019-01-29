545b2a9a2d35bbe579078772cca7654254ac962e
wricciot <38498442+wricciot@users.noreply.github.com>
2019-01-29 12:25:23 +0000

    Query refactoring/1 (#462)
    Fixes #458 
    
    * Solves previously undetected bug in QueryShredding (issue #458, corresponding to issue #25 in Query), pending more tests.
    
    Also fixes regression (wrt #25) in previous commit due to code imported from QueryShredding into Query.
    
    * Merges common query code from Queryshredding into Query.
    Factorizes non-shredding Query code into Queryplain.
    
    * Fixes call to Query.type_of_expression in Queryplain by removing the outermost list type constructor.
    Merges with most recent changes in the master branch.
    
    * Moves query modules to their own directory, with more meaningful names.
    
    * Removes more trailing whitespace.
    
    * Cleans some unnecessary double semi-colons.

--- core/evalir.ml
+++ core/evalir.ml
@@ -672,7 +672,7 @@ struct
             Some (Value.unbox_int (value env limit), Value.unbox_int (value env offset)) in
        if Settings.get_value Basicsettings.Shredding.shredding then
          begin
-           match Queryshredding.compile_shredded env (range, e) with
+           match EvalNestedQuery.compile_shredded env (range, e) with
            | None -> computation env cont e
            | Some (db, p) ->
              begin
@@ -687,16 +687,16 @@ struct
                let execute_shredded_raw (q, t) =
                  Database.execute_select_result (get_fields t) q db, t in
                let raw_results =
-                 Queryshredding.Shred.pmap execute_shredded_raw p in
+                 EvalNestedQuery.Shred.pmap execute_shredded_raw p in
                let mapped_results =
-                 Queryshredding.Shred.pmap Queryshredding.Stitch.build_stitch_map raw_results in
+                 EvalNestedQuery.Shred.pmap EvalNestedQuery.Stitch.build_stitch_map raw_results in
                  apply_cont cont env
-                 (Queryshredding.Stitch.stitch_mapped_query mapped_results)
+                 (EvalNestedQuery.Stitch.stitch_mapped_query mapped_results)
              end
          end
        else (* shredding disabled *)
          begin
-           match Query.compile env (range, e) with
+           match EvalQuery.compile env (range, e) with
            | None -> computation env cont e
            | Some (db, q, t) ->
                let (fieldMap, _, _), _ =
@@ -722,7 +722,7 @@ struct
                                         | _ -> assert false) fields)
           | _ -> assert false in
       let update_query =
-        Query.compile_update db env ((Var.var_of_binder xb, table, field_types), where, body) in
+        EvalQuery.compile_update db env ((Var.var_of_binder xb, table, field_types), where, body) in
       let () = ignore (Database.execute_command update_query db) in
         apply_cont cont env (`Record [])
     | `Delete ((xb, source), where) ->
@@ -734,7 +734,7 @@ struct
                                         | _ -> assert false) fields)
           | _ -> assert false in
       let delete_query =
-        Query.compile_delete db env ((Var.var_of_binder xb, table, field_types), where) in
+        EvalQuery.compile_delete db env ((Var.var_of_binder xb, table, field_types), where) in
       let () = ignore (Database.execute_command delete_query db) in
         apply_cont cont env (`Record [])
     | `CallCC f ->
---
+++ core/query/query.mli
@@ -0,0 +1,72 @@
+open Utility
+
+type base_type = [ `Bool | `Char | `Float | `Int | `String ]
+
+type tag = int
+  [@@deriving show]
+
+type t =
+    [ `For of tag option * (Var.var * t) list * t list * t
+    | `If of t * t * t
+    | `Table of Value.table
+    | `Database of (Value.database * string)
+    | `Singleton of t | `Concat of t list
+    | `Record of t StringMap.t | `Project of t * string | `Erase of t * StringSet.t
+    | `Variant of string * t
+    | `XML of Value.xmlitem
+    | `Apply of string * t list
+    | `Closure of (Ir.var list * Ir.computation) * env
+    | `Primitive of string
+    | `Var of (Var.var * Types.datatype StringMap.t) | `Constant of Constant.constant ]
+and env = Value.env * t Env.Int.t
+  [@@deriving show]
+
+val unbox_xml : [> `XML of 'a ] -> 'a
+
+val unbox_pair : [> `Record of 'a Utility.StringMap.t ] -> 'a * 'a
+
+val unbox_list : ([> `Concat of 'a list | `Singleton of 'b ] as 'a) -> 'b list
+
+val unbox_string : [> `Concat of [> `Concat of 'a
+                                  | `Singleton of [> `Constant of [> `Char of char ] ] as 'b ]
+                list as 'a
+            | `Constant of [> `String of string ]
+            | `Singleton of 'b ] ->
+		string
+
+val used_database : t -> Value.database option
+
+val string_of_t : t -> string
+
+val type_of_expression : t -> Types.datatype
+
+val default_of_base_type : Types.base_type -> [> `Constant of Types.base_type ]
+
+val value_of_expression : ([> `Concat of [> `Singleton of 'a ] list
+             | `Constant of Types.base_type
+             | `Record of 'a Utility.StringMap.t
+             | `Table of Value.table
+             | `Variant of string * 'a
+             | `XML of Value.xmlitem ]
+            as 'a) ->
+           Value.t
+
+val labels_of_field_types : 'a Utility.StringMap.t -> Utility.StringSet.t
+val record_field_types : Types.datatype -> Types.datatype StringMap.t
+val table_field_types : Value.table -> Types.typ Utility.StringMap.t
+val is_list : [> `Concat of 'a
+            | `For of 'b
+            | `If of 'c * 'd * [> `Concat of 'e list ]
+            | `Singleton of 'f
+            | `Table of 'g ] -> bool
+
+module Eval :
+sig
+  val env_of_value_env : 'a -> 'a * 'b Env.Int.t
+  val bind : 'a * 'b Env.Int.t -> Env.Int.name * 'b -> 'a * 'b Env.Int.t
+  val eta_expand_var : Var.var * Types.datatype StringMap.t -> t
+  val computation : Value.t Value.Env.t * t Env.Int.t -> Ir.computation -> t
+  val reduce_where_then : t * t -> t
+  val reduce_and : t * t -> t
+  val eval : Value.t Value.Env.t -> Ir.computation -> t
+end
