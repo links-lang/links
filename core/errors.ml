0581656581a9282edb93289edb55825ea593efcc
Rudi Horn <dyn-git@rudi-horn.de>
2019-02-26 10:34:07 +0000

    removed function which was implemented in `SourceCode` module

--- core/errors.ml
+++ core/errors.ml
@@ -24,11 +24,6 @@ let show_pos : Position.t -> string =
 let prefix_lines prefix s =
   prefix ^ Str.global_replace (Str.regexp "\n") ("\n" ^ prefix) s
 
-let resolve_pos_expr pos =
-  let open Position.Resolved in
-  let pos = resolve pos in
-  start pos, source_expression pos
-
 let format_exception =
   function
   | RichSyntaxError s ->
@@ -37,31 +32,31 @@ let format_exception =
        ^ s.message ^ "\n" ^ prefix_lines "  " s.linetext ^ "\n"
        ^ "   " ^ s.marker)
   | SugarError (pos, s) ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         ("*** Syntactic sugar error: " ^ pos.pos_fname ^ ":"
          ^ string_of_int pos.pos_lnum ^ "\n"
          ^ prefix_lines "   " (s ^ "\nIn expression: " ^ expr ^ "\n"))
   | Getopt.Error s -> s
   | Type_error (pos, s) ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         Printf.sprintf "%s:%d: Type error: %s\nIn expression: %s.\n"
           pos.pos_fname pos.pos_lnum s expr
   | UnboundTyCon (pos, tycon) ->
-      let pos, _ = resolve_pos_expr pos in
+      let pos, _ = Position.resolve_start_expr pos in
       Printf.sprintf "%s:%d: Unbound type constructor %s\n"
                     pos.pos_fname pos.pos_lnum tycon
   | Runtime_error s -> "*** Runtime error: " ^ s
   | Position.ASTSyntaxError (pos, s) ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         Printf.sprintf "%s:%d: Syntax error: %s\nIn expression: %s\n"
           pos.pos_fname pos.pos_lnum s expr
   | Sugartypes.RedundantPatternMatch pos ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         Printf.sprintf "%s:%d: Redundant pattern match:\nIn expression: %s\n"
           pos.pos_fname pos.pos_lnum expr
   | Sugartypes.PatternDuplicateNameError(pos, name) ->
       (* BUG: this can't be right (the pattern and the expression are the same!) *)
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
       let pattern = expr in
         Printf.sprintf
           "%s:%d: Syntax Error: Duplicate name `%s' in pattern\n  %s\nIn expression: %s"
@@ -83,7 +78,7 @@ let format_exception_html = function
        ^ s.message ^ "</p><pre>" ^ xml_escape s.linetext ^ "\n"
        ^ s.marker ^ "</pre>")
   | SugarError (pos, s) ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         ("<h1>Links syntactic sugar error</h1>\n<p>Syntactic sugar error at <code>"
          ^ pos.pos_fname ^"</code>:"
          ^ string_of_int pos.pos_lnum ^ ":</p> <p>"
@@ -91,16 +86,16 @@ let format_exception_html = function
          ^ xml_escape expr ^ "</pre>\n")
   | Getopt.Error s -> s
   | Type_error (pos, s) ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         Printf.sprintf ("<h1>Links type error</h1>\n<p>Type error at <code>%s</code>:%d:</p> <p>%s</p><p>In expression:</p>\n<pre>%s</pre>\n")
           pos.pos_fname pos.pos_lnum s (xml_escape expr)
   | UnboundTyCon (pos, tycon) ->
-      let pos, _ = resolve_pos_expr pos in
+      let pos, _ = Position.resolve_start_expr pos in
         Printf.sprintf ("<h1>Links type error</h1>\n<p>Type error at <code>%s</code>:%d:</p> <p>Unbound type constructor:</p>\n<pre>%s</pre>\n")
           pos.pos_fname pos.pos_lnum tycon
   | MultiplyDefinedToplevelNames duplicates ->
       let show_pos : Position.t -> string = fun pos ->
-        let pos, _ = resolve_pos_expr pos in
+        let pos, _ = Position.resolve_start_expr pos in
         Printf.sprintf "file <code>%s</code>, line %d" pos.Lexing.pos_fname pos.Lexing.pos_lnum
       in
         "<h1>Links Syntax Error</h1><p>Duplicate top-level bindings:</p><ul>" ^
@@ -114,12 +109,12 @@ let format_exception_html = function
 
   | Runtime_error s -> "<h1>Links Runtime Error</h1> " ^ s
   | Position.ASTSyntaxError (pos, s) ->
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
         Printf.sprintf "<h1>Links Syntax Error</h1> Syntax error at <code>%s</code> line %d. %s\nIn expression: <code>%s</code>\n"
           pos.pos_fname pos.pos_lnum s (xml_escape expr)
   | Sugartypes.PatternDuplicateNameError(pos, name) ->
       (* BUG: this can't be right (the pattern and the expression are the same!) *)
-      let pos, expr = resolve_pos_expr pos in
+      let pos, expr = Position.resolve_start_expr pos in
       let pattern = expr in
         Printf.sprintf
           "<h1>Links Syntax Error</h1> <p><code>%s</code> line %d:</p><p>Duplicate name <code>%s</code> in pattern\n<code>%s</code>.</p>\n<p>In expression: <code>%s</code></p>"
