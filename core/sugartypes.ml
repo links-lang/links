9eb0c48372e19bc8a24f541e7156e0f9bd659cea
Rudi Horn <dyn-git@rudi-horn.de>
2019-03-13 11:34:17 +0000

    fixed incorrect assert false for fun deps lens phrase

--- core/sugartypes.ml
+++ core/sugartypes.ml
@@ -389,8 +389,7 @@ struct
     | TupleLit ps -> union_map phrase ps
 
     | LensLit (l, _) -> phrase l
-    (* this should be converted to `LensLit during typeSugar *)
-    | LensFunDepsLit _ -> assert false
+    | LensFunDepsLit (l, _, _) -> phrase l
     | LensKeysLit (l, _, _) -> phrase l
     | LensSelectLit (l, _, _) -> phrase l
     | LensDropLit (l, _, _, _, _) -> phrase l
