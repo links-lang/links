d67cd8c4243c9bd559edb666c924d25d7407df6d
Frank Emrich <frank.emrich@ed.ac.uk>
2019-03-27 15:35:27 +0000

    Saner printing of sugar ASTs; settings for dumping ASTs before and after frontend pipeline (#535)

--- core/frontend.ml
+++ core/frontend.ml
@@ -36,7 +36,7 @@ struct
   let after_typing_ext enabled = extension enabled after_typing
   let before_typing_ext enabled f x = if enabled then f x else x
 
-  let program =
+  let program_pipeline =
     fun tyenv pos_context program ->
       let program = (ResolvePositions.resolve_positions pos_context)#program program in
       let program = DesugarAlienBlocks.transform_alien_blocks program in
@@ -75,7 +75,19 @@ struct
         program), ffi_files)
 
 
-  let interactive =
+let program tyenv pos_context program =
+  if Settings.get_value Basicsettings.show_pre_frontend_ast then
+    Debug.print ("Pre-Frontend AST:\n" ^ Sugartypes.show_program program);
+
+  let ((post_program, _, _), _) as result = program_pipeline tyenv pos_context program in
+
+  if Settings.get_value Basicsettings.show_post_frontend_ast then
+    Debug.print ("Post-Frontend AST:\n" ^ Sugartypes.show_program post_program);
+
+  result
+
+
+  let interactive_pipeline =
     fun tyenv pos_context sentence ->
     let sentence = (ResolvePositions.resolve_positions pos_context)#sentence sentence in
     let _sentence = CheckXmlQuasiquotes.checker#sentence sentence in
@@ -95,4 +107,18 @@ struct
        ->- after_typing ((DesugarFormlets.desugar_formlets tyenv)#sentence ->- snd)
        ->- after_typing ((DesugarPages.desugar_pages tyenv)#sentence ->- snd)
        ->- after_typing ((DesugarFuns.desugar_funs tyenv)#sentence ->- snd)) sentence
+
+
+let interactive tyenv pos_context sentence =
+  if Settings.get_value Basicsettings.show_pre_frontend_ast then
+    Debug.print ("Pre-Frontend AST:\n" ^ Sugartypes.show_sentence sentence);
+
+  let (post_sentence, _, _) as result = interactive_pipeline tyenv pos_context sentence in
+
+  if Settings.get_value Basicsettings.show_post_frontend_ast then
+    Debug.print ("Post-Frontend AST:\n" ^ Sugartypes.show_sentence post_sentence);
+
+  result
+
+
 end
