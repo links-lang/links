bbb13dc056f4cd6466cc596bbf24491a6a2bd019
Rudi Horn <dyn-git@rudi-horn.de>
2019-04-03 11:38:19 +0100

    added lens_format documentation and ran ocamlformat over it.

--- lens/lens_format.ml
+++ lens/lens_format.ml
@@ -14,8 +14,7 @@ let pp_newline_list f b v = pp_print_list ~pp_sep:pp_print_newline f b v
 
 let pp_padded ~length fmt f v =
   let str = Format.asprintf "%a" fmt v in
-  let count = Pervasives.max 0 @@ length - String.length str in
+  let count = Pervasives.max 0 @@ (length - String.length str) in
   Format.fprintf f "%s%s" str @@ String.make count ' '
 
-let show_of_pp fmt =
-  Format.asprintf "%a" fmt
+let show_of_pp fmt = Format.asprintf "%a" fmt
