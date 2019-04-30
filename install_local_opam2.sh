9fbf3b34b86f78ea23f0810f9a5006a925328a9a
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2019-04-30 12:35:00 +0100

    A little bit of alpha renaming has never hurt, said nobody ever...

--- install_local_opam2.sh
+++ install_local_opam2.sh
@@ -6,7 +6,7 @@ OPAM_URL="https://github.com/ocaml/opam/releases/download/${OPAM_VERSION}/opam-$
 # Designated home of opam.
 OPAM_HOME_DIR="$HOME/.opam"
 # Name of the local OPAM executable.
-EXEC_NAME="opam${OPAM_VERSION}_local"
+OPAM_EXEC="opam${OPAM_VERSION}_local"
 
 # Abort if the OPAM home directory already exists -- which could
 # indicate that the current installation image isn't clean.
@@ -16,14 +16,14 @@ if [ -e $OPAM_HOME_DIR ]; then
 fi
 
 # Attempt to download the desired OPAM version.
-wget "$OPAM_URL" -O "$EXEC_NAME"
-if [[ $? -ne 0 || ! -f "./$EXEC_NAME" ]]; then
+wget "$OPAM_URL" -O "$OPAM_EXEC"
+if [[ $? -ne 0 || ! -f "./$OPAM_EXEC" ]]; then
     echo "error: download failed $OPAM_URL" >> /dev/stderr
     exit 1
 fi
 
 # Make the script executable for the owner.
-chmod u+x "$EXEC_NAME"
+chmod u+x "$OPAM_EXEC"
 
 # Initialise OPAM.
-eval "$EXEC_NAME init --disable-sandboxing -n"
+eval "$OPAM_EXEC init --disable-sandboxing -n"
