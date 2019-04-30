2ddac2988ab0e5d6243d240467d6aa43c1ded363
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2019-04-30 12:19:12 +0100

    Upgrade to OPAM 2.0.4.
    This commit upgrades the local installation of OPAM to the latest
    version 2.0.4. Furthermore, this commits also makes a few changes to
    the installation script to make it easier to upgrade OPAM in the
    future.

--- install_local_opam2.sh
+++ install_local_opam2.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-OPAM2_URL="https://github.com/ocaml/opam/releases/download/2.0.1/opam-2.0.1-x86_64-linux"
-ROOT_DIR=~/.opam #must not quote for tilde expansion
-EXEC_NAME=opam2_local
+# OPAM version to download.
+OPAM_VERSION="2.0.4"
+OPAM_URL="https://github.com/ocaml/opam/releases/download/${OPAM_VERSION}/opam-${OPAM_VERSION}-x86_64-linux"
+# Designated home of opam.
+OPAM_HOME_DIR="$HOME/.opam"
+# Name of the local OPAM executable.
+EXEC_NAME="opam${OPAM_VERSION}_local"
 
-
-if [ -e $ROOT_DIR ]; then
-    echo "opam root exists, aborting"
+# Abort if the OPAM home directory already exists -- which could
+# indicate that the current installation image isn't clean.
+if [ -e $OPAM_HOME_DIR ]; then
+    echo "opam root exists, aborting" >> /dev/stderr
     exit 1
 fi
 
-wget "$OPAM2_URL" -O "$EXEC_NAME"
+# Attempt to download the desired OPAM version.
+wget "$OPAM_URL" -O "$EXEC_NAME"
+if [[ $? -ne 0 || ! -f "./$EXEC_NAME" ]]; then
+    echo "error: download failed $OPAM_URL" >> /dev/stderr
+    exit 1
+fi
+
+# Make the script executable for the owner.
 chmod u+x "$EXEC_NAME"
-./${EXEC_NAME} init --disable-sandboxing -n
+
+# Initialise OPAM.
+eval "$EXEC_NAME init --disable-sandboxing -n"
