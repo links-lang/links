062cefba5e2765afe113dc7e6e85189b3f405193
Simon Fowler <simon@simonjf.com>
2019-08-30 13:38:49 +0100

    Fix #383
    The issue was that Lwt.catch was not discriminating between request
    types, and was thus responding with a plain HTML page when it should
    have been responding with a b64-encoded JSON string.
    
    This patch ensures that the correct error is thrown in the console.

--- core/webif.ml
+++ core/webif.ml
@@ -87,7 +87,7 @@ struct
       body ^
       "\n  </body></html>\n"
 
-  let should_contain_client_id cgi_args =
+  let is_ajax_call cgi_args =
     (is_remote_call cgi_args) || (is_client_return cgi_args)
 
   (* jcheney: lifted from serve_request, to de-clutter *)
@@ -106,7 +106,7 @@ struct
       Some (Settings.get_value Basicsettings.websocket_url)
       else None
 
-  let resolve_json_state req_data v =
+  let generate_json_state req_data v =
     let client_id = RequestData.get_client_id req_data in
     let json_state = Json.JsonState.empty client_id (get_websocket_url ()) in
     (* Add event handlers *)
@@ -130,7 +130,7 @@ struct
         Debug.print("Doing ClientReturn for client ID " ^ client_id_str);
         Proc.resolve_external_processes arg;
         Eval.apply_cont cont valenv arg >>= fun (_, result) ->
-        let json_state = resolve_json_state req_data result in
+        let json_state = generate_json_state req_data result in
         let result_json = Json.jsonize_value_with_state result json_state in
         Lwt.return ("text/plain", Utility.base64encode result_json)
       | RemoteCall(func, env, args) ->
@@ -149,7 +149,7 @@ struct
           (prerr_endline "Remaining  procs on server after remote call!";
            assert(false));
            *)
-        let json_state = resolve_json_state req_data r in
+        let json_state = generate_json_state req_data r in
         Lwt.return
           ("text/plain",
             Utility.base64encode (Json.jsonize_value_with_state r json_state))
@@ -160,14 +160,31 @@ struct
   let do_request ((valenv, _, _) as env) cgi_args run render_cont render_servercont_cont response_printer =
     let request = parse_request env cgi_args in
     let (>>=) f g = Lwt.bind f g in
+    (* We need to be a bit careful about what we respond here. If we are evaluating
+     * a ServerCont or EvalMain, that is fine -- but we need to construct a b64-encoded
+     * JSON object if we're responding to a ClientReturn or RemoteCall. *)
     Lwt.catch
       (fun () -> perform_request valenv run render_cont render_servercont_cont request )
-      (function
-       | Aborted r -> Lwt.return r
-       | Failure msg as e ->
-          prerr_endline msg;
-          Lwt.return ("text/html; charset=utf-8", error_page (Errors.format_exception_html e))
-       | exc -> Lwt.return ("text/html; charset=utf-8", error_page (Errors.format_exception_html exc)))
-    >>= fun (content_type, content) ->
-    response_printer [("Content-type", content_type)] content
+      (fun e ->
+        if (is_ajax_call cgi_args) then
+          let formatted_exn =
+            Errors.format_exception e
+            |> Json.js_dq_escape_string in
+          let error_json =
+            "{ \"error\": \"" ^ formatted_exn ^ "\"}" in
+          Lwt.return
+            ("text/plain", Utility.base64encode (error_json))
+        else
+          begin
+            let mime_type = "text/html; charset=utf-8" in
+            match e with
+             | Aborted r -> Lwt.return r
+             | Failure msg as e ->
+                prerr_endline msg;
+                Lwt.return (mime_type, error_page (Errors.format_exception_html e))
+             | exc ->
+                 Lwt.return (mime_type, error_page (Errors.format_exception_html exc))
+          end) >>=
+    fun (content_type, content) ->
+      response_printer [("Content-type", content_type)] content
 end
