links-lang/links

commit 11542db89fcc33f44434f0903976ac2138e4a8bd

Proposal for simpler handling of unsafe sig and fix for #691 (#864)
* alternative approach to unsafe signatures

* add unsafe flag to IR and use it to decide whether to ignore recursion

* make special tame versions of head and tail

* Fix bug where outer effect environment was used to translate desugared function parameter patterns to IR, leading to effect type mismatch.

core/compilePatterns.ml
@@ -195,7 +195,7 @@ struct
     let eff = lookup_effects env in
       Apply
         (TApp
-           (Variable (NEnv.find "hd" Lib.nenv),
+           (Variable (NEnv.find "##hd" Lib.nenv),
             [(Type, t); (Row, eff)]),
          [v])
 
@@ -203,7 +203,7 @@ struct
     let eff = lookup_effects env in
       Apply
         (TApp
-           (Variable (NEnv.find "tl" Lib.nenv),
+           (Variable (NEnv.find "##tl" Lib.nenv),
             [(Type, t); (Row, eff)]),
          [v])
 end

core/lib.ml
@@ -523,6 +523,25 @@ let env : (string * (located_primitive * Types.datatype * pure)) list = [
    datatype "([a]) ~> [a]",
   IMPURE);
 
+  (* HACK: tame versions of head and tail for use in pattern matching *)
+  "##hd",
+  (p1 (fun lst ->
+        match (Value.unbox_list lst) with
+          | [] -> runtime_error "hd() of empty list"
+          | x :: _ -> x
+      ),
+   datatype "([a]) -> a",
+  IMPURE);
+
+  "##tl",
+  (p1 (fun lst ->
+         match (Value.unbox_list lst) with
+            | [] -> runtime_error "tl() of empty list"
+            | _x :: xs -> Value.box_list xs
+      ),
+   datatype "([a]) -> [a]",
+  IMPURE);
+
   "length",
   (p1 (Value.unbox_list ->- List.length ->- Value.box_int),
    datatype "([a]) -> Int",

core/buildTables.ml
@@ -7,7 +7,7 @@ struct
   type t = (Var.var, Ir.eval_fun_def) Hashtbl.t
 
   let make_eval_def : Ir.fun_def -> Ir.eval_fun_def =
-    fun ((_f, info), (_tyvars, xs, body), z, location) ->
+    fun ((_f, info), (_tyvars, xs, body), z, location, _) ->
       info, (List.map Var.var_of_binder xs, body), opt_map Var.var_of_binder z, location
 
   let add fs def =
@@ -196,7 +196,7 @@ struct
             (* we record the relevant free variables of the body *)
             o#close x fvs;
             o#close_cont (IntSet.union fvs fvs') bs
-        | Fun (f, (_tyvars, xs, body), z, _)::bs ->
+        | Fun (f, (_tyvars, xs, body), z, _, _)::bs ->
             let fvs = IntSet.remove (Var.var_of_binder f) fvs in
             let xs = match z with None -> xs | Some z -> z :: xs in
             let bound_vars =
@@ -210,7 +210,7 @@ struct
         | Rec defs::bs ->
             let fvs, bound_vars =
               List.fold_right
-                (fun (f, (_tyvars, xs, _body), z, _) (fvs, bound_vars) ->
+                (fun (f, (_tyvars, xs, _body), z, _, _unsafe) (fvs, bound_vars) ->
                    let f = Var.var_of_binder f in
                    let fvs = IntSet.remove f fvs in
                    let xs = match z with None -> xs | Some z -> z :: xs in
@@ -226,7 +226,7 @@ struct
 
             let fvs' =
               List.fold_left
-                (fun fvs' (_f, (_tyvars, _xs, body), _zs, _location) ->
+                (fun fvs' (_f, (_tyvars, _xs, body), _zs, _location, _unsafe) ->
                    IntSet.union fvs' (FreeVars.computation o#get_type_environment bound_vars body))
                 (IntSet.empty)
                 defs in

core/channelVarUtils.ml
@@ -44,7 +44,7 @@ let variables_in_computation comp =
         traverse_value scrutinee;
         traverse_stringmap (fun (_, c) -> traverse_computation c) cases;
         OptionUtils.opt_iter (fun (_, c) -> traverse_computation c) case_opt
-  and traverse_fundef (bnd, (_, _, _), _, _) =
+  and traverse_fundef (bnd, (_, _, _), _, _, _) =
     let fun_var = Var.var_of_binder bnd in
     match Tables.(lookup fun_defs fun_var) with
       | Some (_, _, (Some fvs_var), _) ->

core/closures.ml
@@ -211,7 +211,7 @@ struct
           let (b, o) = o#super_binding b in
           let o = List.fold_left (fun o q -> o#quantifier_remove q) o quantifiers in
           (b, o)
-        | (Fun (f, (tyvars, xs, body), None, location)) as b
+        | (Fun (f, (tyvars, xs, body), None, location, unsafe)) as b
              when Scope.isLocal (Ir.binding_scope b) ->
           (* reset free and bound variables to be empty *)
           let o = o#reset in
@@ -251,9 +251,9 @@ struct
           let f, o = o#binder f in
           let o = o#register_fun (Var.var_of_binder f) fenv_entry in
           (*Debug.print ("fenv: " ^ show_fenv o#get_fenv);*)
-          Fun (f, (tyvars, xs, body), None, location), o
+          Fun (f, (tyvars, xs, body), None, location, unsafe), o
 
-        | (Fun (_, (tyvars, _, _),_,_)) as b (* global *) ->
+        | (Fun (_, (tyvars, _, _),_,_,_)) as b (* global *) ->
           let o = List.fold_left (fun o q -> o#quantifier q) o tyvars in
           let (b, o) = o#super_binding b in
           let o = List.fold_left (fun o q -> o#quantifier_remove q) o tyvars in
@@ -272,7 +272,7 @@ struct
              in the function type itself) *)
           let _, o =
             List.fold_right
-              (fun (f, _, _, _) (fs, o) ->
+              (fun (f, _, _, _, _) (fs, o) ->
                  let f, o = o#binder f in
                  (f::fs, o))
               defs
@@ -284,7 +284,7 @@ struct
              subsequent ones *)
           let defs, o =
             List.fold_left
-              (fun (defs, (o : 'self)) (f, (tyvars, xs, body), none, location) ->
+              (fun (defs, (o : 'self)) (f, (tyvars, xs, body), none, location, unsafe) ->
                  assert (none = None);
                  let o = List.fold_left (fun o q -> o#quantifier q) o tyvars in
                  let xs, o =
@@ -297,7 +297,7 @@ struct
                  let body, _, o = o#computation body in
                  let o = List.fold_left (fun o q -> o#quantifier_remove q) o tyvars in
 
-                 (f, (tyvars, xs, body), None, location)::defs, o)
+                 (f, (tyvars, xs, body), None, location, unsafe)::defs, o)
               ([], o)
               defs in
 
@@ -308,14 +308,14 @@ struct
           (* ensure functions are in scope for the continuation *)
           let _, o =
             List.fold_right
-              (fun (f, _, _, _) (fs, o) ->
+              (fun (f, _, _, _, _) (fs, o) ->
                  let f, o = o#binder f in
                  (f::fs, o))
               defs
               ([], o) in
 
           let o = List.fold_left
-              (fun o (f, _, _, _) ->
+              (fun o (f, _, _, _, _) ->
                  o#register_fun (Var.var_of_binder f) fenv_entry) o defs in
           let defs = List.rev defs in
           Rec defs, o
@@ -329,7 +329,7 @@ struct
              this global mutually recursive definition. *)
           let _, o =
             List.fold_right
-              (fun (f, _, _, _) (fs, o) ->
+              (fun (f, _, _, _, _) (fs, o) ->
                  let f, o = o#super_binder f in
                  (f::fs, o))
               defs
@@ -337,7 +337,7 @@ struct
 
           let defs, o =
               List.fold_left
-                (fun (defs, (o : 'self_type)) (f, (tyvars, xs, body), none, location) ->
+                (fun (defs, (o : 'self_type)) (f, (tyvars, xs, body), none, location, unsafe) ->
                    assert (none = None);
                    let o = List.fold_left (fun o q -> o#quantifier q) o tyvars in
                    let xs, o =
@@ -349,7 +349,7 @@ struct
                        ([], o) in
                    let body, _, o = o#computation body in
                    let o = List.fold_left (fun o q -> o#quantifier_remove q) o tyvars in
-                   (f, (tyvars, xs, body), None, location)::defs, o)
+                   (f, (tyvars, xs, body), None, location, unsafe)::defs, o)
                 ([], o)
                 defs
           in
@@ -358,7 +358,7 @@ struct
                treat them as globals *)
           let _, o =
             List.fold_right
-              (fun (f, _, _, _) (fs, o) ->
+              (fun (f, _, _, _, _) (fs, o) ->
                  let f, o = o#binder f in
                  (f::fs, o))
               defs
@@ -388,7 +388,7 @@ end
 module Globalise =
 struct
   let binder (x, (t, name, _)) = (x, (t, name, Scope.Global))
-  let fun_def (f, lam, z, location) = (binder f, lam, z, location)
+  let fun_def (f, lam, z, location, unsafe) = (binder f, lam, z, location, unsafe)
   let binding = function
     | Let (x, body) -> Let (binder x, body)
     | Fun def -> Fun (fun_def def)
@@ -479,7 +479,7 @@ struct
           let bs', o = o#pop_hoisted_bindings in
           let bs, o = o#bindings bs in
           bs' @ (b :: bs), o
-        | Fun ((f, _) as fb, (tyvars, xs, body), None, location) :: bs ->
+        | Fun ((f, _) as fb, (tyvars, xs, body), None, location, unsafe) :: bs ->
           assert (Scope.isLocal (Var.scope_of_binder fb));
           let fb = Globalise.binder fb in
           let (xs, o) =
@@ -524,7 +524,7 @@ struct
           let body, _, o = o#computation body in
           let o = o#set_context parents' parent_env' cvars' in
           let fb, o = o#binder (o# generalise_function_type_for_hoisting fb) in
-          let fundef = o#generalise_function_body_for_hoisting (fb, (tyvars, xs, body), zb, location) in
+          let fundef = o#generalise_function_body_for_hoisting (fb, (tyvars, xs, body), zb, location, unsafe) in
           let o = o#push_binding (Fun  fundef) in
           let bs, o = o#bindings bs in
           bs, o
@@ -534,17 +534,17 @@ struct
                function bodies *)
             let fbs, defs, o =
               List.fold_right
-                (fun (f, (tyvars, xs, body), zb, location) (fs, defs,  o) ->
+                (fun (f, (tyvars, xs, body), zb, location, unsafe) (fs, defs,  o) ->
                    (* We have generalise the function's type here, but its body will only be generalised later on *)
                    let f, o = o#binder (o#generalise_function_type_for_hoisting f) in
-                   let def = (f, (tyvars, xs, body), zb, location) in
+                   let def = (f, (tyvars, xs, body), zb, location, unsafe) in
                      (f::fs, def::defs, o))
                 defs
                 ([], [], o) in
 
             let defs, o =
               List.fold_left
-                (fun (defs, (o : 'self)) ((f, _) as fb, (tyvars, xs, body), none, location) ->
+                (fun (defs, (o : 'self)) ((f, _) as fb, (tyvars, xs, body), none, location, unsafe) ->
                    assert (none = None);
                    assert (Scope.isLocal (Var.scope_of_binder fb));
                    let fb = Globalise.binder fb in
@@ -581,7 +581,7 @@ struct
                        Some zb, o#set_context fbs z cvars in
                    let body, _, o = o#computation body in
                    let o = o#set_context parents' parent_env' cvars' in
-                   let fundef = o#generalise_function_body_for_hoisting (fb, (tyvars, xs, body), zb, location) in
+                   let fundef = o#generalise_function_body_for_hoisting (fb, (tyvars, xs, body), zb, location, unsafe) in
                    fundef::defs, o)
                 ([], o)
                 defs in
@@ -648,7 +648,7 @@ struct
 
 
       method generalise_function_body_for_hoisting : Ir.fun_def ->  Ir.fun_def = fun fundef ->
-        let (f, (tyvars, xs, body), z, location) = fundef in
+        let (f, (tyvars, xs, body), z, location, unsafe) = fundef in
         let f_var = Var.var_of_binder f in
         let free_type_vars = (IntMap.find f_var fenv).typevars in
 
@@ -669,9 +669,9 @@ struct
                 let newtype = Instantiate.datatype inner_maps (Var.type_of_binder x) in
                 (Var.update_type newtype x)::xs
               ) xs [] in
-            (* Debug.print ("function currently being hoisted, before instantiation:\n" ^ Ir.string_of_binding (Fun (f, (tyvars, xs, body), z, location))); *)
+            (* Debug.print ("function currently being hoisted, before instantiation:\n" ^ Ir.string_of_binding (Fun (f, (tyvars, xs, body), z, location, unsafe))); *)
             let body = IrTraversals.InstantiateTypes.computation (o#get_type_environment) inner_maps body in
-            (f, (tyvars, xs, body), z, location)
+            (f, (tyvars, xs, body), z, location, unsafe)
           end
 
 

core/ir.ml
@@ -57,7 +57,7 @@ and tail_computation =
 
   | Case       of value * (binder * computation) name_map * (binder * computation) option
   | If         of value * computation * computation
-and fun_def = binder * (tyvar list * binder list * computation) * binder option * location
+and fun_def = binder * (tyvar list * binder list * computation) * binder option * location * bool
 and binding =
   | Let        of binder * (tyvar list * tail_computation)
   | Fun        of fun_def
@@ -103,13 +103,13 @@ and lens_predicate = Static of Lens.Phrase.t | Dynamic of value
 let binding_scope : binding -> scope =
   function
   | Let (b, _)
-  | Fun (b, _, _, _)
-  | Rec ((b, _, _, _)::_)
+  | Fun (b, _, _, _, _)
+  | Rec ((b, _, _, _, _)::_)
   | Alien { binder = b; _ } -> Var.scope_of_binder b
   | Rec []
   | Module _ -> assert false
 
-let binder_of_fun_def (fb, _, _, _) = fb
+let binder_of_fun_def (fb, _, _, _,_) = fb
 
 let tapp (v, tyargs) =
   match tyargs with

core/ir.mli
@@ -59,7 +59,7 @@ and tail_computation =
   | Special    of special
   | Case       of value * (binder * computation) name_map * (binder * computation) option
   | If         of value * computation * computation
-and fun_def = binder * (tyvar list * binder list * computation) * binder option * location
+and fun_def = binder * (tyvar list * binder list * computation) * binder option * location * bool
 and binding =
   | Let        of binder * (tyvar list * tail_computation)
   | Fun        of fun_def

core/irCheck.ml
@@ -1213,7 +1213,7 @@ struct
             let x, o = o#binder x in
             Let (x, (tyvars, tc)), o
 
-        | Fun (f, (tyvars, xs, body), z, location) as binding ->
+        | Fun (f, (tyvars, xs, body), z, location, unsafe) as binding ->
            (* It is important that the type annotations of the parameters are
               expressed in terms of the type variables from tyvars (also for rec
               functions) *)
@@ -1248,7 +1248,7 @@ struct
                handle_ir_type_error lazy_check (f, tyvars, xs, body, z, location, o) (SBind binding) in
               let f, o = o#binder f in
               let o = o#add_function_closure_binder (Var.var_of_binder f) (tyvars, z) in
-              Fun (f, (tyvars, xs, body), z, location), o
+              Fun (f, (tyvars, xs, body), z, location, unsafe), o
 
         | Rec defs  as binding ->
             (* it's important to traverse the function binders first in
@@ -1256,10 +1256,10 @@ struct
                function bodies *)
             let defs, o =
               List.fold_right
-                (fun (f, (tyvars, xs, body), z, location) (fs, o) ->
+                (fun (f, (tyvars, xs, body), z, location, unsafe) (fs, o) ->
                    let o = o#add_function_closure_binder (Var.var_of_binder f) (tyvars, z) in
                    let f, o = o#binder f in
-                     ((f, (tyvars, xs, body), z, location)::fs, o))
+                     ((f, (tyvars, xs, body), z, location, unsafe)::fs, o))
                 defs
                 ([], o) in
 
@@ -1267,7 +1267,7 @@ struct
             lazy (
               let defs, o =
               List.fold_left
-                (fun (defs, (o : 'self_type)) ((f, (tyvars, xs, body), z, location)) ->
+                (fun (defs, (o : 'self_type)) ((f, (tyvars, xs, body), z, location, unsafe)) ->
                    let (z, o) = o#optionu (fun o -> o#binder) z in
                    let xs, o =
                      List.fold_right
@@ -1284,14 +1284,14 @@ struct
                                 tyvars
                                 actual_parameter_types
                                 body
-                                true
+                                (not unsafe) (* Treat recursive bindings with unsafe sig as nonrecursive *)
                                 (SBind binding) in
                   let o = o#add_function_closure_binder (Var.var_of_binder f) (tyvars, z) in
                   (* Debug.print ("added " ^ string_of_int (Var.var_of_binder f) ^ " to closure env"); *)
 
                   let o = OptionUtils.opt_app o#remove_binder o z in
                   let o = List.fold_right (fun b o -> o#remove_binder b) xs o in
-                    (f, (tyvars, xs, body), z, location)::defs, o)
+                    (f, (tyvars, xs, body), z, location, unsafe)::defs, o)
                 ([], o)
                 defs in
               let defs = List.rev defs in

commit d82939b077bb01158f425d8d472a5066ec6878a3

Fix web bug introduced by #864
"#" cannot be used in JS identifiers, so this led to problems executing web things.

This patch replaces "##hd" and "##tl" with "$$hd" and "$$tl", since "$"
is valid in JS identifiers. It also adds the necessary "_$$hd/_$$tl"
stubs in jslib.

core/compilePatterns.ml
@@ -195,7 +195,7 @@ struct
     let eff = lookup_effects env in
       Apply
         (TApp
-           (Variable (NEnv.find "##hd" Lib.nenv),
+           (Variable (NEnv.find "$$hd" Lib.nenv),
             [(Type, t); (Row, eff)]),
          [v])
 
@@ -203,7 +203,7 @@ struct
     let eff = lookup_effects env in
       Apply
         (TApp
-           (Variable (NEnv.find "##tl" Lib.nenv),
+           (Variable (NEnv.find "$$tl" Lib.nenv),
             [(Type, t); (Row, eff)]),
          [v])
 end

core/lib.ml
@@ -524,7 +524,7 @@ let env : (string * (located_primitive * Types.datatype * pure)) list = [
   IMPURE);
 
   (* HACK: tame versions of head and tail for use in pattern matching *)
-  "##hd",
+  "$$hd",
   (p1 (fun lst ->
         match (Value.unbox_list lst) with
           | [] -> runtime_error "hd() of empty list"
@@ -533,7 +533,7 @@ let env : (string * (located_primitive * Types.datatype * pure)) list = [
    datatype "([a]) -> a",
   IMPURE);
 
-  "##tl",
+  "$$tl",
   (p1 (fun lst ->
          match (Value.unbox_list lst) with
             | [] -> runtime_error "tl() of empty list"

lib/js/jslib.js
@@ -1494,6 +1494,8 @@ var _empty = LINKEDLIST.empty;
 var _Cons = LINKEDLIST.Cons;
 var _hd = LINKEDLIST.head;
 var _tl = LINKEDLIST.tail;
+var _$$hd = LINKEDLIST.head;
+var _$$tl = LINKEDLIST.tail;
 var _length = LINKEDLIST.length;
 var _take = LINKEDLIST.take;
 var _drop = LINKEDLIST.drop;