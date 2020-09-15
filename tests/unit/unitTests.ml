a849e0fe1dad69e331a8998d83e3be4327bb51c6
Frank Emrich <git@emrich.io>
2020-09-15 17:29:27 +0100

    Test type-checking of closures

--- tests/unit/unitTests.ml
+++ tests/unit/unitTests.ml
@@ -21,7 +21,7 @@ let suites =
          ( "ir"
          >:::
          let open Links_ir_unit_tests in
-         [ Binding.suite; Functions.suite; Effects.suite; Quantifiers.suite ] );
+         [ Binding.suite; Functions.suite; Effects.suite; Quantifiers.suite; Closures.suite ] );
        ]
 
 let () = run_test_tt_main suites
