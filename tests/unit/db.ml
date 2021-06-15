92d5b0430a88e83dad6230b8d64e087c951bc435
Frank Emrich <git@emrich.io>
2021-06-15 18:02:11 +0100

    split database/lens and ir tests in separate files

--- tests/unit/db.ml
+++ tests/unit/db.ml
@@ -18,17 +18,6 @@ let suites =
            UnitTestsLensSort.suite;
            UnitTestsLensAlias.suite;
          ] );
-         ( "ir"
-         >:::
-         let open Links_ir_unit_tests in
-         [
-           Binding.suite;
-           Functions.suite;
-           Effects.suite;
-           Quantifiers.suite;
-           Closures.suite;
-           Variants.suite;
-         ] );
        ]
 
 let () = run_test_tt_main suites
