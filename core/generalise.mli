7be447c606d0782179722818d2c083287c7cece1
Frank Emrich <git@emrich.io>
2021-06-17 18:03:18 +0200

    generalize -> generalise
    Co-authored-by: Daniel Hillerström <1827113+dhil@users.noreply.github.com>

--- core/generalise.mli
+++ core/generalise.mli
@@ -5,8 +5,8 @@ val generalise : ?unwrap:bool -> Types.environment -> Types.datatype
 val generalise_rigid : ?unwrap:bool -> Types.environment -> Types.datatype
                     -> ((Quantifier.t list * Types.type_arg list) * Types.datatype)
 
-(* Like generalise, but does not mutate the generlized type variables by making them rigid.
-  This means that the generlized type variables must be post-processed elsewhere! *)
+(* Like generalise, but does not mutate the generalised type variables by making them rigid.
+  This means that the generalised type variables must be post-processed elsewhere! *)
 val generalise_without_mutation : ?unwrap:bool -> Types.environment -> Types.datatype
               -> ((Quantifier.t list * Types.type_arg list) * Types.datatype)
 
