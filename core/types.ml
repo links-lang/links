3f968d6b60689657d50856b27a4a2b0e8e4e54d9
Samuel Novak <samuel.novak@protonmail.com>
2021-07-08 14:45:01 +0100

    Alias/RecApp sugar handled already, no need in printer

--- core/types.ml
+++ core/types.ml
@@ -3259,38 +3259,14 @@ module RoundtripPrinter : PRETTY_PRINTER = struct
             end)
 
   and alias_recapp : (string * type_arg list * bool) printer
-    =
-    (* let maybe_remove_shared_effect : Context.t -> type_arg list -> type_arg list
-     *     = let extract_row_var r =
-     *         let rv = unwrap_row r |> fst |> extract_row_parts |> snd3 in
-     *         match Unionfind.find rv with
-     *         | Var (vid, _, _) -> Some vid
-     *         | _ -> None
-     *       in
-     *       fun ctx arg_types ->
-     *       match Context.shared_effect ctx with
-     *       | None -> arg_types (\* No shared effect var => return unchanged list *\)
-     *       | Some vid ->
-     *          (\* we have a shared effect var, so maybe we'll need to remove the last
-     *           (by convention, see SharedEffect) argument from this application *\)
-     *          let others, last = ListUtils.unsnoc arg_types in
-     *          match last with
-     *          | (PrimaryKind.Row, (Row _ as r)) ->
-     *             (match extract_row_var r with
-     *              | Some v when v = vid -> others (\* omit the last type argument *\)
-     *              | _ -> arg_types (\* the original list *\)
-     *             )
-     *          | _ -> arg_types (\* the original list *\)
-     *   in *)
-      let open Printer in
+    = let open Printer in
       Printer (fun ctx (name, arg_types, is_dual) buf ->
           (if is_dual then StringBuffer.write buf "~");
           StringBuffer.write buf (Module_hacks.Name.prettify name);
           (match arg_types with
            | [] -> ()
            | _ -> StringBuffer.write buf " (";
-                  (* let arg_types' = maybe_remove_shared_effect ctx arg_types in *)
-                  Printer.concat_items ~sep:"," type_arg arg_types(* ' *) ctx buf;
+                  Printer.concat_items ~sep:"," type_arg arg_types ctx buf;
                   StringBuffer.write buf ")"))
 
   and row_parts : row' printer
