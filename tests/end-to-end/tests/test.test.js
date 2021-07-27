8c2fb1e57fc0764a63de77f8d1744e106d91ce1b
chefyeum <chefyeum@gmail.com>
2021-07-27 10:55:43 +0100

    apply example

--- tests/end-to-end/tests/test.test.js
+++ tests/end-to-end/tests/test.test.js
@@ -1,16 +1,59 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
-require('selenium-webdriver/chrome')
-require('selenium-webdriver/firefox')
 
-const rootURL = 'https://www.mozilla.org/en-US/'
-let driver
+const { webdriver } = require('selenium-webdriver');
+const { assert } = require('assert');
+const path = require('path');
+
+let driver;
 
 beforeAll(async () => {
-  driver = await new Builder().forBrowser('firefox').build()
-})
+  let capabilities;
+  switch (process.env.BROWSER || "chrome") {
+    case "ie": {
+      // HACK: include IEDriver path by nuget
+      const driverPath = path.join(
+        __dirname,
+        "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
+      );
+      process.env.PATH = `${process.env.PATH};${driverPath};`;
+      capabilities = webdriver.Capabilities.ie();
+      capabilities.set("ignoreProtectedModeSettings", true);
+      capabilities.set("ignoreZoomSetting", true);
+      break;
+    }
+    case "safari": {
+      capabilities = webdriver.Capabilities.safari();
+      break;
+    }
+    case "firefox": {
+      require("geckodriver");
+      capabilities = webdriver.Capabilities.firefox();
+      break;
+    }
+    case "chrome": {
+      require("chromedriver");
+      capabilities = webdriver.Capabilities.chrome();
+      capabilities.set("chromeOptions", {
+        args: [
+          "--headless",
+          "--no-sandbox",
+          "--disable-gpu",
+          "--window-size=1980,1200"
+        ]
+      });
+      break;
+    }
+  }
+  driver = await new webdriver.Builder()
+    .withCapabilities(capabilities)
+    .build();
+});
 
-afterAll(async () => driver.quit())
+afterAll(async () => {
+  await driver.quit()
+});
 
-it('initialises the context', async () => {
-  await driver.get(rootURL)
-})
+it("Google", async () => {
+  await driver.get('http://google.com');
+  assert.equal(await driver.getTitle(), "Google");
+});
+
