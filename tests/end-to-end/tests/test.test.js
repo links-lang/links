a5fc985f6c6989169a275cb30af73a619ac9fd37
chefyeum <chefyeum@gmail.com>
2021-07-27 11:08:56 +0100

    add minimal test

--- tests/end-to-end/tests/test.test.js
+++ tests/end-to-end/tests/test.test.js
@@ -1,49 +1,37 @@
 
-const { webdriver } = require('selenium-webdriver');
-const { assert } = require('assert');
-const path = require('path');
+const { Builder, Capabilities } = require('selenium-webdriver');
+const assert = require('assert');
+// const path = require('path');
 
 let driver;
 
 beforeAll(async () => {
   let capabilities;
   switch (process.env.BROWSER || "chrome") {
-    case "ie": {
-      // HACK: include IEDriver path by nuget
-      const driverPath = path.join(
-        __dirname,
-        "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
-      );
-      process.env.PATH = `${process.env.PATH};${driverPath};`;
-      capabilities = webdriver.Capabilities.ie();
-      capabilities.set("ignoreProtectedModeSettings", true);
-      capabilities.set("ignoreZoomSetting", true);
-      break;
-    }
-    case "safari": {
-      capabilities = webdriver.Capabilities.safari();
-      break;
-    }
+    // case "safari": {
+    //   capabilities = Capabilities.safari();
+    //   break;
+    // }
     case "firefox": {
       require("geckodriver");
-      capabilities = webdriver.Capabilities.firefox();
+      capabilities = Capabilities.firefox();
       break;
     }
-    case "chrome": {
-      require("chromedriver");
-      capabilities = webdriver.Capabilities.chrome();
-      capabilities.set("chromeOptions", {
-        args: [
-          "--headless",
-          "--no-sandbox",
-          "--disable-gpu",
-          "--window-size=1980,1200"
-        ]
-      });
-      break;
-    }
+    // case "chrome": {
+    //   require("chromedriver");
+    //   capabilities = Capabilities.chrome();
+    //   capabilities.set("chromeOptions", {
+    //     args: [
+    //       "--headless",
+    //       "--no-sandbox",
+    //       "--disable-gpu",
+    //       "--window-size=1980,1200"
+    //     ]
+    //   });
+    //   break;
+    // }
   }
-  driver = await new webdriver.Builder()
+  driver = await new Builder()
     .withCapabilities(capabilities)
     .build();
 });
