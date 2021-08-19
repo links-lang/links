3034a8d47e4f4eb97ecb3d5f15ee4ceaa9a62f1f
chefyeum <chefyeum@gmail.com>
2021-08-19 15:59:30 +0100

    change auth info

--- dbtest/index.js
+++ dbtest/index.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+    user: 'links',
+    password: 'links',
+    database: 'links'
 });
 
 pgclient.connect();
