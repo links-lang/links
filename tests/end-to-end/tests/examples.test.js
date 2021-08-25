eff69ecbac617e83fde14b61942465a8551610d7
chefyeum <chefyeum@gmail.com>
2021-08-25 10:27:39 +0100

    tidy up

--- tests/end-to-end/tests/examples.test.js
+++ tests/end-to-end/tests/examples.test.js
@@ -20,24 +20,24 @@ beforeAll(async () => {
 afterAll(async () => {
   await driver.quit();
 
-  // TODO: find workaround to kill links process
-  // require('child_process').exec('killall links.exe');
-
   process.kill(-linksServer.pid, 'SIGTERM');
 });
 
 test('Check factorial up to 64', async () => {
   await driver.get(`${DEFAULT_BASE_URL}/examples/factorial.links`);
 
+  const inputBox = By.xpath('/html/body/form/input[1]');
+  const button = By.xpath('/html/body/form/input[2]');
+
   // Type '64' in the text box
   // Due to refresh, the element need to be located twice.
-  await driver.findElement(By.xpath('/html/body/form/input[1]')).sendKeys('6');
-  await driver.findElement(By.xpath('/html/body/form/input[1]')).sendKeys('4');
+  await driver.findElement(inputBox).sendKeys('6');
+  await driver.findElement(inputBox).sendKeys('4');
+  await driver.findElement(button).click();
 
-  await driver.findElement(By.xpath('/html/body/form/input[2]')).click();
-
+  const table = By.xpath('/html/body/table/tbody');
   // Find tables
-  await driver.wait(until.elementsLocated(By.xpath('/html/body/table/tbody')));
+  await driver.wait(until.elementsLocated(table));
   let table = await driver.findElement(By.xpath('/html/body/table/tbody'));
 
   // Find all rows
@@ -68,17 +68,15 @@ test('Test dictSuggestUpdate', async () => {
   const newWordMeaningInput = By.xpath('/html/body/div[2]/div/form/table/tbody/tr[2]/td[2]/textarea');
   const newWordAddButton = By.css('#add > form > table > tbody > tr:nth-child(3) > td > button');
 
-
-
-  //  //Add new word "Dee"
+  // Add new word "Dee"
   await driver.findElement(newWordInput).sendKeys('Dee');
   await driver.findElement(newWordMeaningInput).sendKeys('A very important person');
   await driver.findElement(newWordAddButton).click();
 
-  // confirm search result showing
   // search Dee
   await driver.findElement(searchBar).sendKeys('Dee');
 
+  // confirm search result showing
   await driver.wait(until.elementLocated(searchResultTable));
   const searchResult = await driver.findElement(searchResultTable);
   const tableItems = await searchResult.findElements(By.xpath('*'))
@@ -87,8 +85,8 @@ test('Test dictSuggestUpdate', async () => {
       const definition = await element.findElement(By.xpath('td[2]/span'));
       return [await word.getText(), await definition.getText()];
     }));
-  
+
   Promise.all(tableItems).then(items => {
     expect(items).toContainEqual(['Dee', 'A very important person']);
-  })
+  });
 });
