801ed18306fb58c7b58526233b99e1225668a5f1
Simon Fowler <simon@simonjf.com>
2021-11-19 12:15:09 +0000

    Parser and typechecker wrangling

--- core/typeSugar.ml
+++ core/typeSugar.ml
@@ -415,6 +415,7 @@ sig
 
   val temporal_join_effects : griper
   val temporal_join_body : griper
+  val temporal_accessor : TemporalOperation.t -> griper
 
 end
   = struct
@@ -1587,6 +1588,17 @@ end
       with_but pos
         ("The body of a temporal join must return a list")
         (expr, t)
+
+    let temporal_accessor op ~pos ~t1:(_, actual) ~t2:(_, expected) ~error:_ =
+      build_tyvar_names [actual; expected];
+      let ppr_actual   = show_type actual   in
+      let ppr_expected = show_type expected in
+      die pos ("The argument of " ^ (TemporalOperation.name op) ^ " " ^
+               "was expected to have type "       ^ nli () ^
+                code ppr_expected                 ^ nl  () ^
+               "but has type"                     ^ nli () ^
+                code ppr_actual                   ^ nl  () ^
+               "instead.")
 end
 
 type context = Types.typing_environment = {
@@ -3185,7 +3197,37 @@ let rec type_check : context -> phrase -> phrase * Types.datatype * Usage.t =
                 | _ -> assert false (* Impossible to construct *) in
             DBTemporalJoin (tmp, erase body, Some result_type), result_type,
             (usages body)
-        | TemporalOp (op, target, args) -> failwith "TODO"
+        | TemporalOp (op, target, arguments) ->
+            let open TemporalOperation in
+            let data_ty = T.Record (Types.make_empty_open_row (lin_any, res_base)) in
+            let target = tc target in
+
+            let arguments = List.map tc arguments in
+
+            let accessor_ty tbl field =
+              let expected =
+                let open Temporality in
+                match tbl with
+                  | Transaction -> Types.make_transaction_time_data_type data_ty
+                  | Valid -> Types.make_valid_time_data_type data_ty
+                  | _ -> assert false
+              in
+              let () = unify ~handle:(Gripers.temporal_accessor op)
+                (pos_and_typ target, no_pos expected) in
+              match field with
+                | Data -> data_ty
+                | From | To -> T.Primitive (Primitive.DateTime)
+            in
+
+            let result_ty =
+              match op with
+                | Accessor (tbl, field) -> accessor_ty tbl field
+            in
+            let replacement_usages =
+              List.map usages arguments
+                |> Usage.combine_many in
+            TemporalOp (op, erase target, List.map erase arguments), result_ty,
+            (Usage.combine (usages target) replacement_usages)
         | Query (range, policy, p, _) ->
             let open QueryPolicy in
             let range, outer_effects, range_usages =
