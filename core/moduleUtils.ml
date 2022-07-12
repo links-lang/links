0d833fc5f2c905ae2ee9253d4bbd25f127df2f88
Yi-Zhou-01 <zhouyi000713@gmail.com>
2022-07-12 11:44:09 +0100

    Check core/moduleUtils.ml stdlib setting

--- core/moduleUtils.ml
+++ core/moduleUtils.ml
@@ -23,7 +23,13 @@ let use_stdlib
 
 (* Standard library path *)
 let stdlib_path =
-  Settings.(option ~default:(Some Linkspath.stdlib) "stdlib_path"
+  let dir =
+    match Utility.getenv "LINKS_LIB" with
+    | Some path -> Filename.concat path "stdlib"
+    | None -> ""
+  in
+  Settings.(option ~default:(Some dir) "stdlib_path"
+  (* Settings.(option ~default:(Some Linkspath.stdlib) "stdlib_path" *)
             |> to_string from_string_option
             |> convert Utility.(Sys.expand ->- some)
             |> sync)
