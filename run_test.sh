419c59d1b044aaf4ce706ca395114b6e2126419d
rajdakin <rajdakin@gmail.com>
2025-04-14 12:23:34 +0100

    [WASM] Used the stack switching interpreter for testing again

--- run_test.sh
+++ run_test.sh
@@ -3,54 +3,54 @@
 make -s -j10 || exit $?
 
 echo "Test 1"
-_build/default/bin/links.exe --mode wasm <(echo 'var w = 0.0; fun f(a: Int) { a + 4 } fun g(b,c : Int) { fun h(d: Int) { fun i() { b+c+d } i() } h(b) * f(b) } g(0,0) + 1') -o output_test.wasm || exit $?
-echo -n "Returns 1: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'var w = 0.0; fun f(a: Int) { a + 4 } fun g(b,c : Int) { fun h(d: Int) { fun i() { b+c+d } i() } h(b) * f(b) } g(0,0) + 1') -o output_test.wast || exit $?
+echo '(assert_return (get "w") (f64.const 0)) (assert_return (get "_init_result") (i64.const 1)) (assert_return (invoke "f" (i64.const 0)) (i64.const 4)) (assert_return (invoke "g" (i64.const 1) (i64.const 2)) (i64.const 20))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 2"
-_build/default/bin/links.exe --mode wasm <(echo 'var a = 1; fun f() { 0 } fun g(b: Int) { fun h() { a + b } fun i() { h() } i } g(0)()') -o output_test.wasm || exit $?
-echo -n "Returns 1: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'var a = 1; fun f() { 0 } fun g(b: Int) { fun h() { a + b } fun i() { h() } i } g(0)()') -o output_test.wast || exit $?
+echo '(assert_return (get "a") (i64.const 1)) (assert_return (get "_init_result") (i64.const 1))' >> output_test.wast # Missing: (invoke "g" (i64.const 1))
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 3"
-_build/default/bin/links.exe --mode wasm <(echo 'if (true) 0 else 1') -o output_test.wasm || exit $?
-echo -n "Returns 0: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'if (true) 0 else 1') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i64.const 0))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 4"
-_build/default/bin/links.exe --mode wasm <(echo 'if (true) false else true') -o output_test.wasm || exit $?
-echo -n "Returns false: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'if (true) false else true') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i32.const 0))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 5"
-_build/default/bin/links.exe --mode wasm <(echo 'var b = true; var a = 2; if (b && (a == 1)) false else true') -o output_test.wasm || exit $?
-echo -n "Returns true: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'var b = true; var a = 2; if (b && (a == 1)) false else true') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i32.const 1))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 6"
-_build/default/bin/links.exe --mode wasm <(echo 'var b = false; var a = 2; if (b || (a == 2)) false else true') -o output_test.wasm || exit $?
-echo -n "Returns false: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'var b = false; var a = 2; if (b || (a == 2)) false else true') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i32.const 0))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 7"
-_build/default/bin/links.exe --mode wasm <(echo 'fun f(x : Int) { fun g() { x } g } f(1)()') -o output_test.wasm || exit $?
-echo -n "Returns 1: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --mode wat <(echo 'fun f(x : Int) { fun g() { x } g } f(1)()') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i64.const 1))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 8"
-_build/default/bin/links.exe --enable-handlers --mode wasm <(echo 'handle (5) { case <Foo => k> -> 2 }') -o output_test.wasm || exit $?
-echo -n "Returns 5: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
-_build/default/bin/links.exe --enable-handlers --mode wasm <(echo 'handle (do Foo) { case <Foo => k> -> 2 }') -o output_test.wasm || exit $?
-echo -n "Returns 2: "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --enable-handlers --mode wat <(echo 'handle (5) { case <Foo => k> -> 2 }') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i64.const 5))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --enable-handlers --mode wat <(echo 'handle (do Foo) { case <Foo => k> -> 2 }') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i64.const 2))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
 echo "Test 9"
-_build/default/bin/links.exe --enable-handlers --mode wasm <(echo 'handle (()) { case <Foo => k> -> k(()) }') -o output_test.wasm || exit $?
-echo -n "Returns (): "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
-_build/default/bin/links.exe --enable-handlers --mode wasm <(echo 'handle (do Foo) { case <Foo => k> -> k(()) }') -o output_test.wasm || exit $?
-echo -n "Returns (): "
-../wizard-engine/bin/wizeng -ext:stack-switching -ext:wizeng output_test.wasm || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --enable-handlers --mode wat <(echo 'handle (()) { case <Foo => k> -> k(false) }') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (ref.null none))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
+_build/default/bin/links.exe --set generate_wizard=false --enable-handlers --mode wat <(echo 'handle (do Foo) { case <Foo => k> -> k(false) }') -o output_test.wast || exit $?
+echo '(assert_return (get "_init_result") (i32.const 0))' >> output_test.wast
+../stack-switching/interpreter/wasm output_test.wast || exit $?
 
-rm output_test.wasm
+rm output_test.wast
